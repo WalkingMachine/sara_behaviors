efae7cad38f0d4fd8a15aff7aee6f0e227c0ff94
philippe117 <philippe117@live.fr>
2017-06-01 00:00:10 -0400

    premier ajout de sara say

---
+++ sara_flexbe_states/src/sara_flexbe_states/sara_say.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+from flexbe_core import EventState, Logger
+from std_msgs.msg import String
+
+import rospy
+
+
+class SaraSay(EventState):
+    '''
+    Speech to text analyse mic audio file and output analysed word from it.
+
+    #> words 		object 	Output the recognized speech.
+
+    <= done     Finish job.
+    <= failed   Job as failed.
+    '''
+
+    def __init__(self):
+        # See example_state.py for basic explanations.
+        super(SaraSay, self).__init__(outcomes=['done'],
+                                            input_keys=['say'])
+
+    def execute(self, userdata):
+        # This method is called periodically while the state is active.
+        # Main purpose is to check state conditions and trigger a corresponding outcome.
+        # If no outcome is returned, the state will stay active.
+        self.check_stop()
+        if self._error:
+            return 'failed'
+
+        # POCKET SPHINX ####
+        #
+
+        userdata.words = "Something that work!"
+        Logger.loginfo('Execute.')
+
+        return 'done'  # One of the outcomes declared above.
+
+    def on_enter(self, userdata):
+        # This method is called when the state becomes active, a transition from another state to this one is taken.
+        # It is primarily used to start actions which are associated with this state.
+
+        self.check_stop()
+        Logger.loginfo('Enter STT')
+
+    def check_stop(self):
+        rospy.Subscriber(self._topic, String, self.callback)
+        if self._stop:
+            self._error = True
+            Logger.logwarn('E-STOP ACTIVATED')
+
+    def callback(self, stop):
+        rospy.loginfo("E-STOP: %s", stop.data)
+        self._stop = stop
+        if stop:
+            self._error = True
+            Logger.logwarn('E-STOP ACTIVATED')
