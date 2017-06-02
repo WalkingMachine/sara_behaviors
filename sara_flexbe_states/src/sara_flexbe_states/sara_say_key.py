55e3d47eb6e80f680d2bb0ad0a1991978bd65090
philippe117 <philippe117@live.fr>
2017-06-02 00:54:44 -0400

    ajout de sara_say et de sara_say_key

--- sara_flexbe_states/src/sara_flexbe_states/sara_say_key.py
+++ sara_flexbe_states/src/sara_flexbe_states/sara_say_key.py
@@ -5,29 +5,31 @@ import rospy
 from wm_tts import srv
 from wm_tts import msg
 
-class SaraSay(EventState):
+class SaraSayKey(EventState):
     """
     Make sara say something
 
-    -- sentence     string      what to say
+    -- Format      string   how to say it
     -- emotion     int       how to feel
+    >= sentence              what key to say
 
     <= done                what's said is said
     """
 
-    def __init__(self, sentence, emotion):
+    def __init__(self, Format , emotion):
         """Constructor"""
 
-        super(SaraSay, self).__init__(outcomes = ['done'])
-        self.sentence = sentence
+        super(SaraSayKey, self).__init__(outcomes = ['done'],
+                                      input_keys=['sentence'])
         self.emotion = emotion
+        self.Format = Format
 
     def execute(self, userdata):
         """Wait for action result and return outcome accordingly"""
 
         rospy.wait_for_service('/wm_say')
         req = msg.say
-        req.sentence = str(self.sentence)
+        req.sentence = str(self.Format( userdata.sentence ))
         req.emotion = self.emotion
         serv = rospy.ServiceProxy('/wm_say', srv.say_service )
         serv( req )
