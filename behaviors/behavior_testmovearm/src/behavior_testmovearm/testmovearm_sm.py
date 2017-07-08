2880244a955d459d5137bc04b2fa0e896831c3cc
philippe117 <philippe117@live.fr>
2017-07-08 03:33:41 -0400

    ajout de test movearm

--- behaviors/behavior_testmovearm/src/behavior_testmovearm/testmovearm_sm.py
+++ behaviors/behavior_testmovearm/src/behavior_testmovearm/testmovearm_sm.py
@@ -37,6 +37,7 @@ class TestMoveArmSM(Behavior):
 
         # references to used behaviors
         self.add_behavior(ActionMoveArmSM, 'ActionMoveArm')
+        self.add_behavior(ActionMoveArmSM, 'ActionMoveArm_2')
 
         # Additional initialization code can be added inside the following tags
         # [MANUAL_INIT]
@@ -48,7 +49,7 @@ class TestMoveArmSM(Behavior):
 
 
     def create(self):
-        # x:872 y:482, x:395 y:220
+        # x:872 y:482, x:348 y:332
         _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
         _state_machine.userdata.actionName = "MoveArm"
 
@@ -61,7 +62,7 @@ class TestMoveArmSM(Behavior):
         with _state_machine:
             # x:22 y:466
             OperatableStateMachine.add('generate pose',
-                                        GenPose(x=1, y=1, z=1, roll=0, pitch=0, yaw=0),
+                                        GenPose(x=0.4, y=-0.2, z=0.5, roll=0, pitch=-1.5, yaw=0),
                                         transitions={'done': 'new action form', 'failed': 'failed'},
                                         autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off},
                                         remapping={'pose': 'pose'})
@@ -80,19 +81,54 @@ class TestMoveArmSM(Behavior):
                                         autonomy={'done': Autonomy.Off},
                                         remapping={'Entry': 'actionName', 'FIFO': 'ActionForm'})
 
-            # x:515 y:444
+            # x:522 y:466
             OperatableStateMachine.add('add pose',
                                         FIFO_Add(),
                                         transitions={'done': 'ActionMoveArm'},
                                         autonomy={'done': Autonomy.Off},
                                         remapping={'Entry': 'pose', 'FIFO': 'ActionForm'})
 
-            # x:570 y:353
+            # x:594 y:339
             OperatableStateMachine.add('ActionMoveArm',
                                         self.use_behavior(ActionMoveArmSM, 'ActionMoveArm'),
+                                        transitions={'finished': 'gen pose 2', 'failed': 'failed'},
+                                        autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+                                        remapping={'ActionForm': 'ActionForm'})
+
+            # x:128 y:148
+            OperatableStateMachine.add('gen pose 2',
+                                        GenPose(x=0.2, y=-0.2, z=0.5, roll=0, pitch=-1.5, yaw=0),
+                                        transitions={'done': 'new form', 'failed': 'failed'},
+                                        autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off},
+                                        remapping={'pose': 'pose'})
+
+            # x:303 y:147
+            OperatableStateMachine.add('new form',
+                                        FIFO_New(),
+                                        transitions={'done': 'dd'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'FIFO': 'ActionForm'})
+
+            # x:623 y:146
+            OperatableStateMachine.add('ActionMoveArm_2',
+                                        self.use_behavior(ActionMoveArmSM, 'ActionMoveArm_2'),
                                         transitions={'finished': 'finished', 'failed': 'failed'},
                                         autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
                                         remapping={'ActionForm': 'ActionForm'})
+
+            # x:439 y:112
+            OperatableStateMachine.add('dd',
+                                        FIFO_Add(),
+                                        transitions={'done': 'dss'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'Entry': 'actionName', 'FIFO': 'ActionForm'})
+
+            # x:519 y:196
+            OperatableStateMachine.add('dss',
+                                        FIFO_Add(),
+                                        transitions={'done': 'ActionMoveArm_2'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'Entry': 'pose', 'FIFO': 'ActionForm'})
 
 
         return _state_machine
