2c39fc02eee22ceaececac3d12081370995915b4
philippe117 <philippe117@live.fr>
2017-07-11 19:29:08 -0400

    ajout des structures de base des action_wrappers

---
+++ behaviors/behavior_actionwrapper_pick/src/behavior_actionwrapper_pick/actionwrapper_pick_sm.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+import roslib; roslib.load_manifest('behavior_actionwrapper_pick')
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from flexbe_states.wait_state import WaitState
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Tue Jul 11 2017
+@author: Philippe La Madeleine
+'''
+class ActionWrapper_PickSM(Behavior):
+    '''
+    action wrapper pour pick
+    '''
+
+
+    def __init__(self):
+        super(ActionWrapper_PickSM, self).__init__()
+        self.name = 'ActionWrapper_Pick'
+
+        # parameters of this behavior
+
+        # references to used behaviors
+
+        # Additional initialization code can be added inside the following tags
+        # [MANUAL_INIT]
+        
+        # [/MANUAL_INIT]
+
+        # Behavior comments:
+
+        # O 287 66 
+        # Pick|n1- object|n2- where to find it
+
+
+
+    def create(self):
+        # x:30 y:322, x:130 y:322
+        _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+
+        # Additional creation code can be added inside the following tags
+        # [MANUAL_CREATE]
+        
+        # [/MANUAL_CREATE]
+
+
+        with _state_machine:
+            # x:38 y:115
+            OperatableStateMachine.add('www',
+                                        WaitState(wait_time=1),
+                                        transitions={'done': 'finished'},
+                                        autonomy={'done': Autonomy.Off})
+
+
+        return _state_machine
+
+
+    # Private functions can be added inside the following tags
+    # [MANUAL_FUNC]
+    
+    # [/MANUAL_FUNC]
