0c583db0787367dee65368c128924e6f821fdba8
Lucas Maurice <lucas.maurice@outlook.com>
2017-07-15 21:24:48 -0400

    Add Stop, Clean Shutdown

--- behaviors/behavior_actionwrapper_pick/src/behavior_actionwrapper_pick/actionwrapper_pick_sm.py
+++ behaviors/behavior_actionwrapper_pick/src/behavior_actionwrapper_pick/actionwrapper_pick_sm.py
@@ -21,69 +21,69 @@ Created on Tue Jul 11 2017
 @author: Philippe La Madeleine
 '''
 class ActionWrapper_PickSM(Behavior):
-    '''
-    action wrapper pour pick
-    '''
+	'''
+	action wrapper pour pick
+	'''
 
 
-    def __init__(self):
-        super(ActionWrapper_PickSM, self).__init__()
-        self.name = 'ActionWrapper_Pick'
+	def __init__(self):
+		super(ActionWrapper_PickSM, self).__init__()
+		self.name = 'ActionWrapper_Pick'
 
-        # parameters of this behavior
+		# parameters of this behavior
 
-        # references to used behaviors
+		# references to used behaviors
 
-        # Additional initialization code can be added inside the following tags
-        # [MANUAL_INIT]
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
         
         # [/MANUAL_INIT]
 
-        # Behavior comments:
+		# Behavior comments:
 
-        # O 287 66 
-        # Pick|n1- object|n2- where to find it
+		# O 357 51 
+		# Pick|n1- object|n2- where to find it
 
 
 
-    def create(self):
-        # x:652 y:286, x:750 y:245
-        _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['Action'])
-        _state_machine.userdata.Action = ["Pick","elephant","room"]
+	def create(self):
+		# x:652 y:286, x:750 y:245
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['Action'])
+		_state_machine.userdata.Action = ["Pick","elephant","room"]
 
-        # Additional creation code can be added inside the following tags
-        # [MANUAL_CREATE]
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
         
         # [/MANUAL_CREATE]
 
 
-        with _state_machine:
-            # x:24 y:53
-            OperatableStateMachine.add('cond',
-                                        CheckConditionState(predicate=lambda x: x[2] != None),
-                                        transitions={'true': 'say Pick object', 'false': 'say no object given'},
-                                        autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
-                                        remapping={'input_value': 'Action'})
+		with _state_machine:
+			# x:129 y:29
+			OperatableStateMachine.add('cond',
+										CheckConditionState(predicate=lambda x: x[2] != ''),
+										transitions={'true': 'say Pick object', 'false': 'say no object given'},
+										autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
+										remapping={'input_value': 'Action'})
 
-            # x:18 y:420
-            OperatableStateMachine.add('say Pick object',
-                                        SaraSayKey(Format=lambda x: " in the "+x[2], emotion=1),
-                                        transitions={'done': 'finished'},
-                                        autonomy={'done': Autonomy.Off},
-                                        remapping={'sentence': 'Action'})
+			# x:18 y:420
+			OperatableStateMachine.add('say Pick object',
+										SaraSayKey(Format=lambda x: " in the "+x[2], emotion=1),
+										transitions={'done': 'finished'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'sentence': 'Action'})
 
-            # x:69 y:187
-            OperatableStateMachine.add('say no object given',
-                                        SaraSayKey(Format=lambda x: "I'm going to pick that "+x[1], emotion=1),
-                                        transitions={'done': 'finished'},
-                                        autonomy={'done': Autonomy.Off},
-                                        remapping={'sentence': 'Action'})
-
-
-        return _state_machine
+			# x:192 y:167
+			OperatableStateMachine.add('say no object given',
+										SaraSayKey(Format=lambda x: "I'm going to pick that "+x[1], emotion=1),
+										transitions={'done': 'finished'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'sentence': 'Action'})
 
 
-    # Private functions can be added inside the following tags
-    # [MANUAL_FUNC]
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
     
     # [/MANUAL_FUNC]
