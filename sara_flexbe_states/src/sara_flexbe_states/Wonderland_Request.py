f4bf3c4def0dc3f387ca71b7ec58f54339ea38d5
Lucas Maurice <lucas.maurice@outlook.com>
2017-07-17 01:51:31 -0400

    Clean code

--- sara_flexbe_states/src/sara_flexbe_states/Wonderland_Request.py
+++ sara_flexbe_states/src/sara_flexbe_states/Wonderland_Request.py
@@ -10,28 +10,29 @@ class Wonderland_Request(EventState):
 	Send requests to Wonderland server
 	
 	># url      string  command to read
-	<= response string  Finish job.
+	#< response string  Finish job.
 	
+	<= done     data readed correctly
+	<= error    error while data is reading
 	
 	'''
 	
 	def __init__(self):
-		# See example_state.py for basic explanations.
-		super(Wonderland_Request, self).__init__(outcomes=['done', 'error'],
-		                                         input_keys=['url'],
-		                                         output_keys=['response'])
+		super(Wonderland_Request, self).__init__(outcomes=['done', 'error'], input_keys=['url'], output_keys=['response'])
+		# generate post key for authentication
 		self._header = {'api-key': 'asdf'}
 	
 	def execute(self, userdata):
-		# This method is called periodically while the state is active.
-		# Main purpose is to check state conditions and trigger a corresponding outcome.
-		# If no outcome is returned, the state will stay active.
+		# Generate URL to contact
 		url = "http://192.168.0.46:8000/api/" + userdata.url
+		
+		# try the request
 		try:
 			response = requests.get(url, headers=self._header)
 		except requests.exceptions.RequestException as e:
 			print e
 			return 'error'
-			
+		
+		# read and return content
 		userdata.response = response.content
-		return 'done'  # One of the outcomes declared above.
+		return 'done'
