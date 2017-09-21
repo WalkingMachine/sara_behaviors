9a47a019ed9d0a169b04789e82fde2e483e4ad38
philippe117 <philippe117@live.fr>
2017-09-21 10:55:00 -0400

    premier nétoyage du main behavior

--- behaviors/behavior_actionwrapper_bring/src/behavior_actionwrapper_bring/actionwrapper_bring_sm.py
+++ behaviors/behavior_actionwrapper_bring/src/behavior_actionwrapper_bring/actionwrapper_bring_sm.py
@@ -12,7 +12,6 @@ from flexbe_states.check_condition_state import CheckConditionState
 from sara_flexbe_states.sara_say import SaraSay
 from sara_flexbe_states.sara_say_key import SaraSayKey
 from flexbe_states.calculation_state import CalculationState
-from behavior_go_to_room.go_to_room_sm import Go_To_RoomSM
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -24,98 +23,89 @@ Created on Tue Jul 11 2017
 @author: Philippe La Madeleine
 '''
 class ActionWrapper_BringSM(Behavior):
-	'''
-	action wrapper pour bring
-	'''
+    '''
+    action wrapper pour bring
+    '''
 
 
-	def __init__(self):
-		super(ActionWrapper_BringSM, self).__init__()
-		self.name = 'ActionWrapper_Bring'
+    def __init__(self):
+        super(ActionWrapper_BringSM, self).__init__()
+        self.name = 'ActionWrapper_Bring'
 
-		# parameters of this behavior
+        # parameters of this behavior
 
-		# references to used behaviors
-		self.add_behavior(Go_To_RoomSM, 'Go_To_Room')
+        # references to used behaviors
 
-		# Additional initialization code can be added inside the following tags
-		# [MANUAL_INIT]
+        # Additional initialization code can be added inside the following tags
+        # [MANUAL_INIT]
         
         # [/MANUAL_INIT]
 
-		# Behavior comments:
+        # Behavior comments:
 
-		# O 508 30 
-		# Bring|n1- object|n2- area|n3- beneficiary
+        # O 508 30 
+        # Bring|n1- object|n2- area|n3- beneficiary
 
 
 
-	def create(self):
-		# x:868 y:291, x:857 y:562
-		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['Action', 'index'])
-		_state_machine.userdata.Action = ["Bring","love","the world"]
-		_state_machine.userdata.index = 0
+    def create(self):
+        # x:868 y:291, x:857 y:562
+        _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['Action'])
+        _state_machine.userdata.Action = ["Bring","love","the world"]
 
-		# Additional creation code can be added inside the following tags
-		# [MANUAL_CREATE]
+        # Additional creation code can be added inside the following tags
+        # [MANUAL_CREATE]
         
         # [/MANUAL_CREATE]
 
 
-		with _state_machine:
-			# x:24 y:30
-			OperatableStateMachine.add('cond',
-										CheckConditionState(predicate=lambda x: x[1] != ''),
-										transitions={'true': 'cond2', 'false': 'say no object given'},
-										autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
-										remapping={'input_value': 'Action'})
+        with _state_machine:
+            # x:24 y:30
+            OperatableStateMachine.add('cond',
+                                        CheckConditionState(predicate=lambda x: x[1] != ''),
+                                        transitions={'true': 'cond2', 'false': 'say no object given'},
+                                        autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
+                                        remapping={'input_value': 'Action'})
 
-			# x:266 y:85
-			OperatableStateMachine.add('say no object given',
-										SaraSay(sentence="You didn't told me what to bring", emotion=1),
-										transitions={'done': 'finished'},
-										autonomy={'done': Autonomy.Off})
+            # x:266 y:85
+            OperatableStateMachine.add('say no object given',
+                                        SaraSay(sentence="You didn't told me what to bring", emotion=1),
+                                        transitions={'done': 'finished'},
+                                        autonomy={'done': Autonomy.Off})
 
-			# x:53 y:176
-			OperatableStateMachine.add('cond2',
-										CheckConditionState(predicate=lambda x: x[2] != ''),
-										transitions={'true': 'say3', 'false': 'say22'},
-										autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
-										remapping={'input_value': 'Action'})
+            # x:53 y:176
+            OperatableStateMachine.add('cond2',
+                                        CheckConditionState(predicate=lambda x: x[2] != ''),
+                                        transitions={'true': 'say3', 'false': 'say22'},
+                                        autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
+                                        remapping={'input_value': 'Action'})
 
-			# x:84 y:485
-			OperatableStateMachine.add('say3',
-										SaraSayKey(Format=lambda x: "I'm now gonna bring the "+x[1]+" to "+x[2], emotion=1),
-										transitions={'done': 'callculate name'},
-										autonomy={'done': Autonomy.Off},
-										remapping={'sentence': 'Action'})
+            # x:84 y:485
+            OperatableStateMachine.add('say3',
+                                        SaraSayKey(Format=lambda x: "I'm now gonna bring the "+x[1]+" to "+x[2], emotion=1),
+                                        transitions={'done': 'callculate name'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'sentence': 'Action'})
 
-			# x:133 y:279
-			OperatableStateMachine.add('say22',
-										SaraSayKey(Format=lambda x: "I'm now gonna bring the "+x[1], emotion=1),
-										transitions={'done': 'callculate name'},
-										autonomy={'done': Autonomy.Off},
-										remapping={'sentence': 'Action'})
+            # x:133 y:279
+            OperatableStateMachine.add('say22',
+                                        SaraSayKey(Format=lambda x: "I'm now gonna bring the "+x[1], emotion=1),
+                                        transitions={'done': 'callculate name'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'sentence': 'Action'})
 
-			# x:354 y:321
-			OperatableStateMachine.add('callculate name',
-										CalculationState(calculation=lambda x: x[1]),
-										transitions={'done': 'Go_To_Room'},
-										autonomy={'done': Autonomy.Off},
-										remapping={'input_value': 'Action', 'output_value': 'name'})
-
-			# x:567 y:335
-			OperatableStateMachine.add('Go_To_Room',
-										self.use_behavior(Go_To_RoomSM, 'Go_To_Room'),
-										transitions={'finished': 'Go_To_Room', 'failed': 'failed'},
-										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
-										remapping={'name': 'name', 'index': 'index'})
+            # x:354 y:321
+            OperatableStateMachine.add('callculate name',
+                                        CalculationState(calculation=lambda x: x[1]),
+                                        transitions={'done': 'finished'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'input_value': 'Action', 'output_value': 'name'})
 
 
-		return _state_machine
+        return _state_machine
 
 
-	# Private functions can be added inside the following tags
-	# [MANUAL_FUNC]
+    # Private functions can be added inside the following tags
+    # [MANUAL_FUNC]
     
     # [/MANUAL_FUNC]
--- behaviors/behavior_actionwrapper_move/src/behavior_actionwrapper_move/actionwrapper_move_sm.py
+++ behaviors/behavior_actionwrapper_move/src/behavior_actionwrapper_move/actionwrapper_move_sm.py
@@ -12,7 +12,6 @@ from flexbe_states.check_condition_state import CheckConditionState
 from sara_flexbe_states.sara_say_key import SaraSayKey
 from sara_flexbe_states.sara_say import SaraSay
 from flexbe_states.wait_state import WaitState
-from behavior_go_to_room.go_to_room_sm import Go_To_RoomSM
 from flexbe_states.calculation_state import CalculationState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
@@ -25,127 +24,131 @@ Created on Tue Jul 11 2017
 @author: Philippe La Madeleine
 '''
 class ActionWrapper_MoveSM(Behavior):
-	'''
-	action wrapper pour move_base
-	'''
+    '''
+    action wrapper pour move_base
+    '''
 
 
-	def __init__(self):
-		super(ActionWrapper_MoveSM, self).__init__()
-		self.name = 'ActionWrapper_Move'
+    def __init__(self):
+        super(ActionWrapper_MoveSM, self).__init__()
+        self.name = 'ActionWrapper_Move'
 
-		# parameters of this behavior
+        # parameters of this behavior
 
-		# references to used behaviors
-		self.add_behavior(Go_To_RoomSM, 'Go_To_Room')
+        # references to used behaviors
 
-		# Additional initialization code can be added inside the following tags
-		# [MANUAL_INIT]
+        # Additional initialization code can be added inside the following tags
+        # [MANUAL_INIT]
         
         # [/MANUAL_INIT]
 
-		# Behavior comments:
+        # Behavior comments:
 
-		# O 664 17 
-		# move|n1- where to go|n2- direction to go (oferriden by 1-)|n3- distance to go (oferriden by 1-)
+        # O 664 17 
+        # move|n1- where to go|n2- direction to go (oferriden by 1-)|n3- distance to go (oferriden by 1-)
 
 
 
-	def create(self):
-		# x:822 y:356, x:821 y:464
-		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['Action', 'index'])
-		_state_machine.userdata.Action = ['Move','','forward','1 meter']
-		_state_machine.userdata.x = ""
-		_state_machine.userdata.y = ""
-		_state_machine.userdata.z = ""
-		_state_machine.userdata.theta = ""
-		_state_machine.userdata.waypoint_id = ""
-		_state_machine.userdata.pose = ""
-		_state_machine.userdata.index = 0
+    def create(self):
+        # x:822 y:356, x:821 y:464
+        _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['Action'])
+        _state_machine.userdata.Action = ['Move','','forward','1 meter']
+        _state_machine.userdata.x = ""
+        _state_machine.userdata.y = ""
+        _state_machine.userdata.z = ""
+        _state_machine.userdata.theta = ""
+        _state_machine.userdata.waypoint_id = ""
+        _state_machine.userdata.pose = ""
 
-		# Additional creation code can be added inside the following tags
-		# [MANUAL_CREATE]
+        # Additional creation code can be added inside the following tags
+        # [MANUAL_CREATE]
         
         # [/MANUAL_CREATE]
 
-		# x:30 y:322, x:130 y:322
-		_sm_move_vector_0 = OperatableStateMachine(outcomes=['finished', 'failed'])
+        # x:30 y:325
+        _sm_move_to_location_0 = OperatableStateMachine(outcomes=['done'], input_keys=['Action'])
 
-		with _sm_move_vector_0:
-			# x:52 y:136
-			OperatableStateMachine.add('wait',
-										WaitState(wait_time=2),
-										transitions={'done': 'finished'},
-										autonomy={'done': Autonomy.Off})
-
-
-
-		with _state_machine:
-			# x:21 y:122
-			OperatableStateMachine.add('check',
-										CheckConditionState(predicate=lambda x: x[1] != ''),
-										transitions={'true': 'say area', 'false': 'cond1'},
-										autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
-										remapping={'input_value': 'Action'})
-
-			# x:187 y:328
-			OperatableStateMachine.add('say vector',
-										SaraSayKey(Format=lambda x: "I'm now going to move "+x[2]+x[3], emotion=1),
-										transitions={'done': 'move vector'},
-										autonomy={'done': Autonomy.Off},
-										remapping={'sentence': 'Action'})
-
-			# x:230 y:127
-			OperatableStateMachine.add('say area',
-										SaraSayKey(Format=lambda x: "I'm now going to the "+x[1], emotion=1),
-										transitions={'done': 'calculate'},
-										autonomy={'done': Autonomy.Off},
-										remapping={'sentence': 'Action'})
-
-			# x:15 y:329
-			OperatableStateMachine.add('cond1',
-										CheckConditionState(predicate=lambda x: x[2] != ''),
-										transitions={'true': 'say vector', 'false': 'say no info'},
-										autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
-										remapping={'input_value': 'Action'})
-
-			# x:35 y:521
-			OperatableStateMachine.add('say no info',
-										SaraSay(sentence="You didn't told me where to go", emotion=1),
-										transitions={'done': 'say no goal given'},
-										autonomy={'done': Autonomy.Off})
-
-			# x:373 y:528
-			OperatableStateMachine.add('say no goal given',
-										SaraSay(sentence="I'm lost now because of you", emotion=1),
-										transitions={'done': 'finished'},
-										autonomy={'done': Autonomy.Off})
-
-			# x:366 y:323
-			OperatableStateMachine.add('move vector',
-										_sm_move_vector_0,
-										transitions={'finished': 'finished', 'failed': 'failed'},
-										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
-
-			# x:683 y:126
-			OperatableStateMachine.add('Go_To_Room',
-										self.use_behavior(Go_To_RoomSM, 'Go_To_Room'),
-										transitions={'finished': 'finished', 'failed': 'failed'},
-										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
-										remapping={'name': 'name', 'index': 'index'})
-
-			# x:430 y:120
-			OperatableStateMachine.add('calculate',
-										CalculationState(calculation=lambda x: x[1]),
-										transitions={'done': 'Go_To_Room'},
-										autonomy={'done': Autonomy.Off},
-										remapping={'input_value': 'Action', 'output_value': 'name'})
+        with _sm_move_to_location_0:
+            # x:30 y:40
+            OperatableStateMachine.add('Get name of location',
+                                        CalculationState(calculation=lambda x: x[1]),
+                                        transitions={'done': 'done'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'input_value': 'Action', 'output_value': 'name'})
 
 
-		return _state_machine
+        # x:30 y:322, x:130 y:322
+        _sm_move_vector_1 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['Action'])
+
+        with _sm_move_vector_1:
+            # x:52 y:136
+            OperatableStateMachine.add('wait',
+                                        WaitState(wait_time=2),
+                                        transitions={'done': 'finished'},
+                                        autonomy={'done': Autonomy.Off})
 
 
-	# Private functions can be added inside the following tags
-	# [MANUAL_FUNC]
+
+        with _state_machine:
+            # x:21 y:122
+            OperatableStateMachine.add('check',
+                                        CheckConditionState(predicate=lambda x: x[1] != ''),
+                                        transitions={'true': 'say area', 'false': 'cond1'},
+                                        autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
+                                        remapping={'input_value': 'Action'})
+
+            # x:187 y:328
+            OperatableStateMachine.add('say vector',
+                                        SaraSayKey(Format=lambda x: "I'm now going to move "+x[2]+x[3], emotion=1),
+                                        transitions={'done': 'move vector'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'sentence': 'Action'})
+
+            # x:230 y:127
+            OperatableStateMachine.add('say area',
+                                        SaraSayKey(Format=lambda x: "I'm now going to the "+x[1], emotion=1),
+                                        transitions={'done': 'move to location'},
+                                        autonomy={'done': Autonomy.Off},
+                                        remapping={'sentence': 'Action'})
+
+            # x:15 y:329
+            OperatableStateMachine.add('cond1',
+                                        CheckConditionState(predicate=lambda x: x[2] != ''),
+                                        transitions={'true': 'say vector', 'false': 'say no info'},
+                                        autonomy={'true': Autonomy.Off, 'false': Autonomy.Off},
+                                        remapping={'input_value': 'Action'})
+
+            # x:35 y:521
+            OperatableStateMachine.add('say no info',
+                                        SaraSay(sentence="You didn't told me where to go", emotion=1),
+                                        transitions={'done': 'say no goal given'},
+                                        autonomy={'done': Autonomy.Off})
+
+            # x:373 y:528
+            OperatableStateMachine.add('say no goal given',
+                                        SaraSay(sentence="I'm lost now because of you", emotion=1),
+                                        transitions={'done': 'finished'},
+                                        autonomy={'done': Autonomy.Off})
+
+            # x:366 y:323
+            OperatableStateMachine.add('move vector',
+                                        _sm_move_vector_1,
+                                        transitions={'finished': 'finished', 'failed': 'failed'},
+                                        autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+                                        remapping={'Action': 'Action'})
+
+            # x:362 y:125
+            OperatableStateMachine.add('move to location',
+                                        _sm_move_to_location_0,
+                                        transitions={'done': 'finished'},
+                                        autonomy={'done': Autonomy.Inherit},
+                                        remapping={'Action': 'Action'})
+
+
+        return _state_machine
+
+
+    # Private functions can be added inside the following tags
+    # [MANUAL_FUNC]
     
     # [/MANUAL_FUNC]
