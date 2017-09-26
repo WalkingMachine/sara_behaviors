9e3b9b064633df712ec4362b4f3473cf77ea43e4
philippe117 <philippe117@live.fr>
2017-09-26 09:42:20 -0400

    reformatage du code

--- sara_flexbe_states/src/sara_flexbe_states/get_robot_pose.py
+++ sara_flexbe_states/src/sara_flexbe_states/get_robot_pose.py
@@ -14,7 +14,7 @@ import json
 
 
 class Get_Robot_Pose(EventState):
-	'''
+    '''
 	Gets the latest message on the given topic and stores it to userdata.
 
 	-- topic 		string		The topic on which should be listened.
@@ -29,59 +29,59 @@ class Get_Robot_Pose(EventState):
 
 	'''
 
-	def __init__(self, blocking=True, clear=False):
-		'''
+    def __init__(self, blocking=True, clear=False):
+        '''
 		Constructor
 		'''
-		super(Get_Robot_Pose, self).__init__(outcomes=['done', 'failed'],
-		                                      output_keys=['pose'])
+        super(Get_Robot_Pose, self).__init__(outcomes=['done', 'failed'],
+                                             output_keys=['pose'])
 
-		self._topic = "/robot_pose"
-		self._blocking = blocking
-		self._clear = clear
-		self._connected = False
+        self._topic = "/robot_pose"
+        self._blocking = blocking
+        self._clear = clear
+        self._connected = False
 
-		(msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic)
+        (msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic)
 
-		if msg_topic == self._topic:
-			msg_type = self._get_msg_from_path(msg_path)
-			self._sub = ProxySubscriberCached({self._topic: msg_type})
-			self._connected = True
-		else:
-			Logger.logwarn(
-				'Topic %s for state %s not yet available.\nFound: %s\nWill try again when entering the state...' % (
-				self._topic, self.name, str(msg_topic)))
+        if msg_topic == self._topic:
+            msg_type = self._get_msg_from_path(msg_path)
+            self._sub = ProxySubscriberCached({self._topic: msg_type})
+            self._connected = True
+        else:
+            Logger.logwarn(
+                'Topic %s for state %s not yet available.\nFound: %s\nWill try again when entering the state...' % (
+                    self._topic, self.name, str(msg_topic)))
 
-	def execute(self, userdata):
-		'''
+    def execute(self, userdata):
+        '''
 		Execute this state
 		'''
-		if not self._connected:
-			return 'failed'
+        if not self._connected:
+            return 'failed'
 
-		if self._sub.has_msg(self._topic) or not self._blocking:
-			userdata.pose = self._sub.get_last_msg(self._topic)
-			self._sub.remove_last_msg(self._topic)
-			return 'done'
+        if self._sub.has_msg(self._topic) or not self._blocking:
+            userdata.pose = self._sub.get_last_msg(self._topic)
+            self._sub.remove_last_msg(self._topic)
+            return 'done'
 
-	def on_enter(self, userdata):
-		if not self._connected:
-			(msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic)
-			if msg_topic == self._topic:
-				msg_type = self._get_msg_from_path(msg_path)
-				self._sub = ProxySubscriberCached({self._topic: msg_type})
-				self._connected = True
-				Logger.loginfo('Successfully subscribed to previously unavailable topic %s' % self._topic)
-			else:
-				Logger.logwarn('Topic %s still not available, giving up.' % self._topic)
+    def on_enter(self, userdata):
+        if not self._connected:
+            (msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic)
+            if msg_topic == self._topic:
+                msg_type = self._get_msg_from_path(msg_path)
+                self._sub = ProxySubscriberCached({self._topic: msg_type})
+                self._connected = True
+                Logger.loginfo('Successfully subscribed to previously unavailable topic %s' % self._topic)
+            else:
+                Logger.logwarn('Topic %s still not available, giving up.' % self._topic)
 
-		if self._connected and self._clear and self._sub.has_msg(self._topic):
-			self._sub.remove_last_msg(self._topic)
+        if self._connected and self._clear and self._sub.has_msg(self._topic):
+            self._sub.remove_last_msg(self._topic)
 
-	def _get_msg_from_path(self, msg_path):
-		msg_import = msg_path.split('/')
-		msg_module = '%s.msg' % (msg_import[0])
-		package = __import__(msg_module, fromlist=[msg_module])
-		clsmembers = inspect.getmembers(package, lambda member: inspect.isclass(member) and member.__module__.endswith(
-			msg_import[1]))
-		return clsmembers[0][1]
+    def _get_msg_from_path(self, msg_path):
+        msg_import = msg_path.split('/')
+        msg_module = '%s.msg' % (msg_import[0])
+        package = __import__(msg_module, fromlist=[msg_module])
+        clsmembers = inspect.getmembers(package, lambda member: inspect.isclass(member) and member.__module__.endswith(
+            msg_import[1]))
+        return clsmembers[0][1]
