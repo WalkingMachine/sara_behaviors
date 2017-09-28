f397cf4c15ccda9a02c6a81f0c110a81d09fffb6
philippe117 <philippe117@live.fr>
2017-09-28 14:20:34 -0400

    ajoute set head angle

---
+++ sara_flexbe_states/src/sara_flexbe_states/sara_set_angle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from flexbe_core import EventState, Logger
+import rospy
+from std_msgs.msg import Float64
+
+
+class SaraSetAngle(EventState):
+    """
+    Make sara change the angle her the head
+
+    -- angle     Float64      angle of the head
+
+    <= done                what's said is said
+    """
+
+
+    def __init__(self, angle):
+        """Constructor"""
+
+        super(SaraSetAngle, self).__init__(outcomes = ['done'])
+        self.angle = angle
+
+    def execute(self, userdata):
+
+        Logger.loginfo('Setting head angle to '+self.angle)
+        pub = rospy.Publisher("/sara_head_pitch_controller/command", Float64, queue_size=1)
+        ms = Float64
+        ms.data = self.angle
+        pub.publish(ms)
+
+        Logger.loginfo('Publishing done')
+
+        return 'done'
