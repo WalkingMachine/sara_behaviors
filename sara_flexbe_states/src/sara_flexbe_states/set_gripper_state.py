acf8f871b6de67e29ac54222e72470d0c4d7cae4
philippe117 <philippe117@live.fr>
2017-10-11 14:16:01 -0400

    refait le get gripper pour detecter et retourner la taille des objets saisis

--- sara_flexbe_states/src/sara_flexbe_states/set_gripper_state.py
+++ sara_flexbe_states/src/sara_flexbe_states/set_gripper_state.py
@@ -2,13 +2,16 @@
 
 
 from flexbe_core import EventState, Logger
-from flexbe_core.proxy import ProxyPublisher
-from control_msgs.msg import _GripperCommandActionGoal, GripperCommandActionGoal
+from flexbe_core.proxy import ProxyPublisher, ProxySubscriberCached
+from control_msgs.msg import GripperCommandActionGoal, GripperCommandActionFeedback
+import rostopic
+from rospy.rostime import get_time
+import inspect
 
 '''
 Created on 31.01.2017
 
-@author: Alberto Romay
+@author: Philippe La Madeleine
 '''
 
 
@@ -16,10 +19,12 @@ class SetGripperState(EventState):
     '''
     Publishes a command for the gripper.
 
-    >= width      float        0-255 desired gripper width.
-    >= effort     float        0-255 desired gripper effort.
+    >= width        float        desired gripper width in meter.
+    >= effort       float        desired gripper effort.
+    <= object_size   float       result width.
 
-    <= done       Done publishing.
+    <# object       an object has been gripped
+    ># no_object    no object gripped
 
     '''
 
@@ -27,13 +32,32 @@ class SetGripperState(EventState):
         '''
         Constructor
         '''
-        super(SetGripperState, self).__init__(outcomes=['done'], input_keys=['width','effort'])
+        super(SetGripperState, self).__init__(outcomes=['object', 'no_object'], input_keys=['width','effort'], output_keys=['object_size'])
 
         self._topic = 'sara_gripper_action_controller/gripper_cmd/goal'
+        self._topic = 'sara_gripper_action_controller/gripper_cmd/feedback'
         self._pub = ProxyPublisher({self._topic: GripperCommandActionGoal})
+        self.sub = ProxySubscriberCached
+        (msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic)
+
+        if msg_topic == self._topic:
+            msg_type = self._get_msg_from_path(msg_path)
+            self._sub = ProxySubscriberCached({self._topic: msg_type})
+            self._connected = True
+            Logger.loginfo('connecting marker state to '+self._topic)
+        else:
+            Logger.logwarn('Topic %s for state %s not yet available.\nFound: %s\nWill try again when entering the state...' % (self._topic, self.name, str(msg_topic)))
 
     def execute(self, userdata):
-        return 'done'
+        if self.time-get_time() <= 0:
+            if self._sub.has_msg(self._topic):
+                message = self._sub.get_last_msg(self._topic)
+                userdata.object_size = message.feedback.position()
+                if abs( userdata.object_size-self.width) > 0.001:
+                    return 'object'
+                else:
+                    return 'no_object'
+            return 'object'
 
     def on_enter(self, userdata):
         msg = GripperCommandActionGoal()
@@ -41,3 +65,20 @@ class SetGripperState(EventState):
         msg.goal.command.max_effort = userdata.effort
         self._pub.publish(self._topic, msg)
 
+        self.time = get_time()
+        if not self._connected:
+            (msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic)
+            if msg_topic == self._topic:
+                msg_type = self._get_msg_from_path(msg_path)
+                self._sub = ProxySubscriberCached({self._topic: msg_type})
+                self._connected = True
+                Logger.loginfo('Successfully subscribed to previously unavailable topic %s' % self._topic)
+            else:
+                Logger.logwarn('Topic %s still not available, giving up.' % self._topic)
+
+    def _get_msg_from_path(self, msg_path):
+        msg_import = msg_path.split('/')
+        msg_module = '%s.msg' % (msg_import[0])
+        package = __import__(msg_module, fromlist=[msg_module])
+        clsmembers = inspect.getmembers(package, lambda member: inspect.isclass(member) and member.__module__.endswith(msg_import[1]))
+        return clsmembers[0][1]
