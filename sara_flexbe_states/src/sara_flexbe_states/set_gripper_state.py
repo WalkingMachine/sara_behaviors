b2a4cb882a3c5e394c3b67371e519b731e6e7a2f
WM toto <wm@toto.com>
2017-10-15 00:01:48 -0400

    fix de gripper

--- sara_flexbe_states/src/sara_flexbe_states/set_gripper_state.py
+++ sara_flexbe_states/src/sara_flexbe_states/set_gripper_state.py
@@ -3,9 +3,8 @@
 
 from flexbe_core import EventState, Logger
 from flexbe_core.proxy import ProxyPublisher, ProxySubscriberCached
-from control_msgs.msg import GripperCommandActionGoal, GripperCommandActionFeedback
+from control_msgs.msg import GripperCommandActionGoal
 import rostopic
-from rospy.rostime import get_time
 import inspect
 
 '''
@@ -28,53 +27,62 @@ class SetGripperState(EventState):
 
     '''
 
-    def __init__(self):
+    def __init__(self, width, effort):
         '''
         Constructor
         '''
-        super(SetGripperState, self).__init__(outcomes=['object', 'no_object'], input_keys=['width','effort'], output_keys=['object_size'])
+        super(SetGripperState, self).__init__(outcomes=['object', 'no_object'], output_keys=['object_size'])
 
-        self._topic = 'sara_gripper_action_controller/gripper_cmd/goal'
-        self._topic = 'sara_gripper_action_controller/gripper_cmd/feedback'
+        self.width = width
+        self.effort = effort
+        self._connected  = False
+
+        self._topic = '/sara_gripper_action_controller/gripper_cmd/goal'
+        self._topic_f = '/sara_gripper_action_controller/gripper_cmd/result'
+
         self._pub = ProxyPublisher({self._topic: GripperCommandActionGoal})
-        self.sub = ProxySubscriberCached
-        (msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic)
 
-        if msg_topic == self._topic:
+        (msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic_f)
+        if msg_topic == self._topic_f:
             msg_type = self._get_msg_from_path(msg_path)
-            self._sub = ProxySubscriberCached({self._topic: msg_type})
+            self._sub = ProxySubscriberCached({self._topic_f: msg_type})
             self._connected = True
-            Logger.loginfo('connecting marker state to '+self._topic)
+
+            Logger.loginfo('connecting marker state to '+self._topic_f)
         else:
-            Logger.logwarn('Topic %s for state %s not yet available.\nFound: %s\nWill try again when entering the state...' % (self._topic, self.name, str(msg_topic)))
+            Logger.logwarn('Topic %s for state %s not yet available.\nFound: %s\nWill try again when entering the state...' % (self._topic_f, self.name, str(msg_topic)))
 
     def execute(self, userdata):
-        if self.time-get_time() <= 0:
-            if self._sub.has_msg(self._topic):
-                message = self._sub.get_last_msg(self._topic)
-                userdata.object_size = message.feedback.position()
-                if abs( userdata.object_size-self.width) > 0.001:
-                    return 'object'
-                else:
-                    return 'no_object'
-            return 'object'
+
+        if not self._connected:
+            return 'no_object'
+
+        if self._sub.has_msg(self._topic_f):
+            message = self._sub.get_last_msg(self._topic_f)
+            size = message.result.position
+            self._sub.remove_last_msg(self._topic_f)
+            userdata.object_size = size
+            if abs( size-self.width) > 0.02:
+                return 'object'
+            else:
+                return 'no_object'
 
     def on_enter(self, userdata):
         msg = GripperCommandActionGoal()
-        msg.goal.command.position = userdata.width
-        msg.goal.command.max_effort = userdata.effort
+        msg.goal.command.position = self.width
+        msg.goal.command.max_effort = self.effort
         self._pub.publish(self._topic, msg)
 
-        self.time = get_time()
         if not self._connected:
-            (msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic)
-            if msg_topic == self._topic:
+            (msg_path, msg_topic, fn) = rostopic.get_topic_type(self._topic_f)
+            if msg_topic == self._topic_f:
                 msg_type = self._get_msg_from_path(msg_path)
-                self._sub = ProxySubscriberCached({self._topic: msg_type})
+                self._sub = ProxySubscriberCached({self._topic_f: msg_type})
                 self._connected = True
-                Logger.loginfo('Successfully subscribed to previously unavailable topic %s' % self._topic)
+                Logger.loginfo('Successfully subscribed to previously unavailable topic %s' % self._topic_f)
             else:
-                Logger.logwarn('Topic %s still not available, giving up.' % self._topic)
+                Logger.logwarn('Topic %s still not available, giving up.' % self._topic_f)
+        self._sub.remove_last_msg(self._topic_f)
 
     def _get_msg_from_path(self, msg_path):
         msg_import = msg_path.split('/')
