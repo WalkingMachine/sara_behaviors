563b7edc8a2f4491f9a4e864176533465b94294e
KiwiColoredHorror <duchainer@gmail.com>
2017-10-25 19:19:17 -0400

    Added pose_gen_euler_key.py

---
+++ sara_flexbe_states/src/sara_flexbe_states/pose_gen_euler_key.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from flexbe_core import EventState, Logger
+from tf.transformations import quaternion_from_euler
+from geometry_msgs.msg import Pose
+
+class GenPoseEulerKey(EventState):
+    '''
+    ### State
+    Generate a pose from xyz and euler angles keys instead of parameters
+
+    ### input keys
+    ># x
+    ># y
+    ># z
+    ># yaw
+    ># pitch
+    ># roll
+
+    ### output_keys
+    #> pose generated pose
+
+    ### outcomes
+    <= done
+    '''
+
+    def __init__(self):
+        # See example_state.py for basic explanations.
+        super(GenPoseEulerKey, self).__init__(outcomes=['done'], output_keys=['pose'])
+        self.pt = Pose()
+        self.pt.position.x = userdata.x
+        self.pt.position.y = userdata.y
+        self.pt.position.z = userdata.z
+        qt = quaternion_from_euler(userdata.roll, userdata.pitch, userdata.yaw)
+        self.pt.orientation.w = qt[3]
+        self.pt.orientation.x = qt[0]
+        self.pt.orientation.y = qt[1]
+        self.pt.orientation.z = qt[2]
+
+
+    def execute(self, userdata):
+        userdata.pose = self.pt
+        return 'done'
