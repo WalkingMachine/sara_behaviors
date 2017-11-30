14e8c5bc52c1f1eab14bf7889f7ab7109b3301cb
philippe117 <philippe117@live.fr>
2017-11-30 14:54:06 -0500

    ajoute du multithreading

--- sara_flexbe_states/src/sara_flexbe_states/move_arm_pose.py
+++ sara_flexbe_states/src/sara_flexbe_states/move_arm_pose.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 from flexbe_core import EventState, Logger
 from moveit_commander import MoveGroupCommander
+import threading
 
 class MoveArmPose(EventState):
     '''
@@ -14,34 +15,58 @@ class MoveArmPose(EventState):
     <= failed   Job as failed.
     '''
 
-    def __init__(self, wait=True):
+    def __init__(self, execute=True, wait=True):
         # See example_state.py for basic explanations.
         super(MoveArmPose, self).__init__(outcomes=['done', 'failed'], input_keys=['pose'])
-        self.group = MoveGroupCommander("RightArm")
-        self.plan = None
+        self.execute = execute
         self.wait = wait
-        self.error = False
+        self.thread = None
 
     def execute(self, userdata):
-        if self.error:
-            try:
-                self.plan = self.group.plan()
-            except:
-                return 'failed'
-
-        if self.group.execute(self.plan, wait=self.wait):
-            return 'done'
-        else:
-            return 'failed'
+        if not self.thread.is_alive():
+            return self.thread.outcome
 
     def on_enter(self, userdata):
-        Logger.loginfo('Enter Move Arm')
-        self.group.set_pose_target( userdata.pose )
-        try:
-            self.plan = self.group.plan()
-        except:
-            self.error = True
+        self.thread = self.myThread(1, "moving_arm", 1, userdata, self.wait)
+        self.thread.start()
 
     def on_exit(self, userdata):
 
         self.group.stop()
+
+
+    class myThread(threading.Thread):
+        def __init__(self, threadID, name, counter, userdata, wait):
+            threading.Thread.__init__(self)
+            self.threadID = threadID
+            self.name = name
+            self.counter = counter
+            self.group = MoveGroupCommander("RightArm")
+            self.plan = None
+            self.pose = userdata.pose
+            self.error = False
+            self.wait = wait
+            self.outcome = "run"
+
+
+        def run(self):
+            Logger.loginfo('Enter Move Arm')
+            self.group.set_pose_target(self.pose)
+            try:
+                self.plan = self.group.plan()
+            except:
+                self.error = True
+
+            if self.error:
+                try:
+                    self.plan = self.group.plan()
+                except:
+                    self.outcome = "failed"
+                    return
+
+            if self.group.execute(self.plan, wait=self.wait):
+                self.outcome = "done"
+                return
+            else:
+                self.outcome = "failed"
+                return
