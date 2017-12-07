351a194522f08faaccd0c1e7de3736bd199cb48a
philippe117 <philippe117@live.fr>
2017-12-07 15:27:20 -0500

    wait

--- sara_flexbe_states/src/sara_flexbe_states/move_arm_pose.py
+++ sara_flexbe_states/src/sara_flexbe_states/move_arm_pose.py
@@ -4,6 +4,22 @@ from flexbe_core import EventState, Logger
 from moveit_commander import MoveGroupCommander
 from multiprocessing import Process
 
+
+def MoveArmRun(group, pose, wait):
+    Logger.loginfo('Enter Move Arm')
+    group.set_pose_target(pose)
+    try:
+        plan = group.plan()
+    except:
+        return "failed"
+    Logger.loginfo('Plan done, stating movement')
+
+    if group.execute(plan, wait=wait):
+        return "done"
+    else:
+        return "failed"
+
+
 class MoveArmPose(EventState):
     '''
     MoveArmPose move the arm to a specific pose
@@ -15,6 +31,8 @@ class MoveArmPose(EventState):
     <= failed   Job as failed.
     '''
 
+
+
     def __init__(self, move=True, wait=True):
         # See example_state.py for basic explanations.
         super(MoveArmPose, self).__init__(outcomes=['done', 'failed'], input_keys=['pose'])
@@ -22,40 +40,18 @@ class MoveArmPose(EventState):
         self.wait = wait
         self.thread = None
         self.outcome = None
-        self.error = None
         self.group = MoveGroupCommander("RightArm")
 
     def execute(self, userdata):
         Logger.loginfo('Moving Arm')
-        if self.outcome:
-            return self.outcome
+        if self.thread.is_alive():
+            return self.thread.exitcode
 
     def on_enter(self, userdata):
-        self.thread = Process(target=self.run, args=[userdata])
+        self.thread = Process(target=MoveArmRun, args=[self.group, userdata.pose, self.wait])
         self.thread.start()
-
 
     def on_exit(self, userdata):
         self.group.stop()
 
-    def run(self, userdata):
-        Logger.loginfo('Enter Move Arm')
-        self.group.set_pose_target(userdata.pose)
-        try:
-            self.plan = self.group.plan()
-        except:
-            self.error = True
 
-        if self.error:
-            try:
-                self.plan = self.group.plan()
-            except:
-                self.outcome = "failed"
-                return
-
-        if self.group.execute(self.plan, wait=self.wait):
-            self.outcome = "done"
-            return
-        else:
-            self.outcome = "failed"
-            return
