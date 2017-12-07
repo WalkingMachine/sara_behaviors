c7130d6aa350f91b8923f33613ae8ba3caa4a22c
philippe117 <philippe117@live.fr>
2017-12-07 14:55:46 -0500

    change de technique

--- sara_flexbe_states/src/sara_flexbe_states/move_arm_pose.py
+++ sara_flexbe_states/src/sara_flexbe_states/move_arm_pose.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 from flexbe_core import EventState, Logger
 from moveit_commander import MoveGroupCommander
-import threading
+from multiprocessing import Process
 
 class MoveArmPose(EventState):
     '''
@@ -21,54 +21,41 @@ class MoveArmPose(EventState):
         self.move = move
         self.wait = wait
         self.thread = None
+        self.outcome = None
+        self.error = None
         self.group = MoveGroupCommander("RightArm")
 
     def execute(self, userdata):
         Logger.loginfo('Moving Arm')
-        nb = threading.activeCount()
-        Logger.loginfo(str(nb))
-        if self.thread.outcome:
-            return self.thread.outcome
+        if self.outcome:
+            return self.outcome
 
     def on_enter(self, userdata):
-        self.thread = self.myThread(1, "moving_arm", 1, userdata, self.wait, self.group)
+        self.thread = Process(target=self.run)
         self.thread.start()
+
 
     def on_exit(self, userdata):
         self.group.stop()
 
+    def run(self):
+        Logger.loginfo('Enter Move Arm')
+        self.group.set_pose_target(self.pose)
+        try:
+            self.plan = self.group.plan()
+        except:
+            self.error = True
 
-    class myThread(threading.Thread):
-        def __init__(self, threadID, name, counter, userdata, wait, group):
-            threading.Thread.__init__(self)
-            self.threadID = threadID
-            self.name = name
-            self.counter = counter
-            self.group = group
-            self.plan = None
-            self.pose = userdata.pose
-            self.error = False
-            self.wait = wait
-            self.outcome = None
-
-        def run(self):
-            Logger.loginfo('Enter Move Arm')
-            self.group.set_pose_target(self.pose)
+        if self.error:
             try:
                 self.plan = self.group.plan()
             except:
-                self.error = True
-
-            if self.error:
-                try:
-                    self.plan = self.group.plan()
-                except:
-                    self.outcome = "failed"
-                    return
-
-            if self.group.execute(self.plan, wait=self.wait):
-                self.outcome = "done"
-                return
-            else:
                 self.outcome = "failed"
                 return
+
+        if self.group.execute(self.plan, wait=self.wait):
+            self.outcome = "done"
+            return
+        else:
+            self.outcome = "failed"
+            return
