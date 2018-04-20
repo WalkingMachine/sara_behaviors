4b934ec3e1e9ef7b570d8f158e56fae67a669bfd
philippe <philippe117@live.fr>
2018-04-19 21:11:37 -0400

    compare l'ID des entitées pour retourner celle qui correspond

--- sara_flexbe_states/src/sara_flexbe_states/Get_Entity_By_ID.py
+++ sara_flexbe_states/src/sara_flexbe_states/Get_Entity_By_ID.py
@@ -11,29 +11,24 @@ import math
 
 class GetEntityByID(EventState):
     '''
-        will list people seen by the camera
+        Search for an entity by it's ID number
 
-        -- frontality_level float
-        #> persons          object
+        ># ID              int
+        #> Entity          object
 
         <= found            people are found
         <= not_found        no one is found
 
     '''
 
-    def __init__(self, frontality_level):
+    def __init__(self):
         '''
         Constructor
         '''
-        super(GetEntityByID, self).__init__(outcomes=['found', 'not_found'], output_keys=['Entity'])
+        super(GetEntityByID, self).__init__(outcomes=['found', 'not_found'], input_key=['ID'], output_keys=['Entity'])
         self._sub = ProxySubscriberCached({'/entities': Entities})
 
-        self._topic = "/robot_pose"
-        self._subpos = ProxySubscriberCached({self._topic: Pose})
-        self.frontality_level = frontality_level
-        self.mypose = None
         self.message = None
-
 
     def execute(self, userdata):
 
@@ -41,58 +36,15 @@ class GetEntityByID(EventState):
             self.mypose = userdata.pose = self._subpos.get_last_msg(self._topic)
 
         if self._sub.has_msg('/entities'):
-            Logger.loginfo('getting message')
+            Logger.loginfo('getting list of entities')
             self.message = self._sub.get_last_msg('/entities')
             self._sub.remove_last_msg('/entities')
 
-        if self.message is not None and self.mypose is not None:
-            persons = self.list()
-            userdata.GetEntityByID = persons
-            userdata.number = len(persons)
+        if self.message is not None:
 
-            if len(persons) != 0:
-                return 'found'
-            else:
-                return 'not_found'
+            for entity in self.message.entities:
+                if entity.ID == userdata.ID:
+                    userdata.Entity = entity
+                    return 'found'
 
-    def list(self):
-        persons = []
-        wraps = []
-        for entity in self.message.entities:
-            if entity.name == 'person':
-                wrap = wrapper()
-                wrap.init(self.mypose, entity, self.frontality_level)
-
-                wraps.append(wrap)
-
-        wraps.sort(key=wrapper.key)
-
-        for wrap in wraps:
-            persons.append(wrap.entity)
-
-        return persons
-
-
-class wrapper():
-    def init(self, mypose, entity, frontality_level):
-
-        self.entity = entity
-
-        x = entity.position.x - mypose.position.x
-        y = entity.position.y - mypose.position.y
-
-        quat = [mypose.orientation.x, mypose.orientation.y, mypose.orientation.z, mypose.orientation.w]
-        euler = euler_from_quaternion(quat)
-        A = euler[2]
-
-        a = math.tan(A)
-        b = y - x * a
-
-        self.dist = (abs(y - a * x - b) / (1 + b ** 2) ** 0.5) * frontality_level
-        self.dist += (((entity.position.x - mypose.position.x) ** 2 + (
-                entity.position.y - mypose.position.y) ** 2) ** 0.5) * (1 - frontality_level)
-        self.dist /= entity.probability**2
-
-    def key(self):
-
-        return self.dist
+            return 'not_found'
