644b90d69a10c5aeffae3964c910774ca2eeccb9
Raphael-Duchaine <11084784+Raphael-Duchaine@users.noreply.github.com>
2018-04-26 11:55:24 -0400

    Change state for distance in 2d (only x and y)

--- sara_flexbe_states/src/sara_flexbe_states/get_distance2D.py
+++ sara_flexbe_states/src/sara_flexbe_states/get_distance2D.py
@@ -33,4 +33,4 @@ class getDistance(EventState):
         return 'done'
 
     def calculate_distance(p1,p2):
-        return math.sqrt(math.pow(p2.x-p1.x,2)+math.pow(p2.y-p1.y,2)+math.pow(p2.z-p1.z,2))
+        return math.sqrt(math.pow(p2.x-p1.x,2)+math.pow(p2.y-p1.y,2))
