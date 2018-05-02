c1548046cc368af0c59d96ee37788e6a235f135f
philippe <philippe117@live.fr>
2018-05-01 22:12:29 -0400

    ajoute les states pour stocker et recupèrer des rosparams

---
+++ sara_flexbe_states/src/sara_flexbe_states/GetRosParam.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python
+
+import rospy
+from flexbe_core import EventState, Logger
+
+'''
+Created on 21.09.2017
+@author: Philippe La Madeleine
+'''
+
+
+class SetRosParam(EventState):
+    '''
+    Get a value from the ros parameter server.
+    -- ParamName    string      The desired value.
+    
+    #> Value      object      The rosparam to set.
+    
+    <= done                 The rosparam is set
+    '''
+
+    def __init__(self, ParamName):
+        '''
+        Constructor
+        '''
+        super(SetRosParam, self).__init__(outcomes=['done'], output_keys=['Value'])
+        self.ParamName = ParamName
+
+    def execute(self, userdata):
+        '''
+        Execute this state
+        '''
+
+        userdata.Value = rospy.get_param(self.ParamName)
+        return "done"
