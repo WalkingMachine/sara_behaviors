ca22d11e8490b1dd00a921bb32ee38f1dfc8e1d3
Lucas Maurice <lucas.maurice@outlook.com>
2018-05-15 15:00:55 -0400

    Implement request for get entities from wonderland.

--- sara_flexbe_states/src/sara_flexbe_states/WonderlandGetEntityVerbal.py
+++ sara_flexbe_states/src/sara_flexbe_states/WonderlandGetEntityVerbal.py
@@ -5,95 +5,68 @@ import json
 
 import requests
 from flexbe_core import EventState
+from rospy import logerr, logwarn, loginfo
+from sara_msgs.msg import Entity, Entities
 
 
-class WonderlandEntityNameByID(EventState):
+class WonderlandGetEntityVerbal(EventState):
     '''
     Read the position of a room in a json string
-    ># id                int     id of the room
+    ># name             string         Recognition name of the object
+    ># containers       string array   Array of containers recognition name (can be empty) on single container
 
-    #> entityClass       string   of the entity
-    #> entityName        string   of the entity
-    #> entityCategory    string   of the entity
-    #> entityColor       string   of the entity
-    #> entityWeight      string   of the entity
-    #> entitySize        string   of the entity
-    #> entityContainer   string   of the entity
+    #> entities         sara_msgs/Entities
 
-    #> entityPosX        float   position of the entity
-    #> entityPosY        float   position of the entity
-    #> entityPosZ        float   position of the entity
-    #> entityPosYaw      float   position of the entity
-    #> entityPosPitch    float   position of the entity
-    #> entityPosRoll     float   position of the entity
-
-    #> entityWaypointX   float   position of the position to reach for have the object
-    #> entityWaypointY   float   position of the position to reach for have the object
-    #> entityWaypointYaw float   position of the position to reach for have the object
-
-    #> depth_waypoint    float   position of the room
-    #> depth_position    float   position of the room
-
-    <= done              return when at least one entity exist
-    <= none              return when no entity have the selected name
-    <= error             return when error reading data
+    <= one              return when one entity exist
+    <= multiple         return when more than one entity exist
+    <= none             return when no entity exist
+    <= error            return when error reading data
     '''
 
     def __init__(self):
         # See example_state.py for basic explanations.
-        super(WonderlandEntityNameByID, self).__init__(outcomes=['done', 'none', 'error'],
-                                                       input_keys=['id'],
-                                                       output_keys=['entityClass', 'entityName', 'entityCategory',
-                                                                    'entityColor', 'entityWeight', 'entitySize',
-                                                                    'entityContainer', 'entityPosX', 'entityPosY',
-                                                                    'entityPosZ', 'entityPosYaw', 'entityPosPitch',
-                                                                    'entityPosRoll', 'entityWaypointX',
-                                                                    'entityWaypointY', 'entityWaypointYaw'])
+        super(WonderlandGetEntityVerbal, self).__init__(outcomes=['one', 'multiple', 'none', 'error'],
+                                                        input_keys=['name', 'containers'],
+                                                        output_keys=['entities'])
 
     def execute(self, userdata):
         # Generate URL to contact
-        url = "http://wonderland:8000/api/entity?entityId=" + str(userdata.id)
 
-        print("This")
-        print("Is")
-        print("A")
-        print("Test")
+        if type(userdata.name) is str:
+            url = "http://wonderland:8000/api/entity?entityClass=" + str(userdata.name)
+        else:
+            url = "http://wonderland:8000/api/entity?none"
+
+        if type(userdata.containers) is str:
+            url += "&entityContainer=" + userdata.containers
+
+        elif type(userdata.containers) is list:
+            for container in userdata.containers:
+                if type(container) == "str":
+                    url += "&entityContainer=" + container
+
+        logwarn(url)
 
         # try the request
         try:
             response = requests.get(url)
-            print(response)
         except requests.exceptions.RequestException as e:
-            print(e)
+            logerr(e)
             return 'error'
 
         # parse parameter json data
         data = json.loads(response.content)
 
-        if not 'entityClass' in data:
+        loginfo(data)
+
+        if len(data) == 0:
             return 'none'
+        elif len(data) == 1:
+            entity = Entity()
+            userdata.entities = entity
+            return 'one'
 
-        # write return data
-        userdata.entityClass = data['entityClass']
-        userdata.entityName = data['entityName']
-        userdata.entityCategory = data['entityCategory']
-        userdata.entityColor = data['entityColor']
-        userdata.entityWeight = data['entityWeight']
-        userdata.entitySize = data['entitySize']
-        userdata.entityContainer = data['entityContainer']
-
-        userdata.entityPosX = data['entityPosX']
-        userdata.entityPosY = data['entityPosY']
-        userdata.entityPosZ = data['entityPosZ']
-        userdata.entityPosYaw = data['entityPosYaw']
-        userdata.entityPosPitch = data['entityPosPitch']
-        userdata.entityPosRoll = data['entityPosRoll']
-
-        userdata.entityWaypointX = data['entityWaypointX']
-        userdata.entityWaypointY = data['entityWaypointY']
-        userdata.entityWaypointYaw = data['entityWaypointYaw']
-
-        userdata.depth_waypoint = data['depth_waypoint']
-        userdata.depth_position = data['depth_position']
-
-        return 'done'
+        else:
+            entities = Entities()
+            userdata.entities = entities
+            return 'multiple'
