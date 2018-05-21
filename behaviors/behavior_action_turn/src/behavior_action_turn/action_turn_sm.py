d7e0643c58dcf4ca58fdb93fe53d52e5674ee280
KiwiColoredHorror <duchainer@gmail.com>
2018-05-21 00:21:42 -0400

    Add action_turn

---
+++ behaviors/behavior_action_turn/src/behavior_action_turn/action_turn_sm.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+import roslib; roslib.load_manifest('behavior_action_turn')
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from flexbe_states.wait_state import WaitState
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Mon May 21 2018
+@author: Raphael Duchaine
+'''
+class action_turnSM(Behavior):
+    '''
+    Quality of life action
+
+Ask for a rotation in degree with forward as 0deg
+Right = -90deg , Left = 90deg
+    '''
+
+
+    def __init__(self):
+        super(action_turnSM, self).__init__()
+        self.name = 'action_turn'
+
+        # parameters of this behavior
+
+        # references to used behaviors
+
+        # Additional initialization code can be added inside the following tags
+        # [MANUAL_INIT]
+        
+        # [/MANUAL_INIT]
+
+        # Behavior comments:
+
+
+
+    def create(self):
+        # x:30 y:293, x:130 y:293
+        _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+
+        # Additional creation code can be added inside the following tags
+        # [MANUAL_CREATE]
+        
+        # [/MANUAL_CREATE]
+
+
+        with _state_machine:
+            # x:45 y:74
+            OperatableStateMachine.add('placeHolder',
+                                        WaitState(wait_time=0),
+                                        transitions={'done': 'finished'},
+                                        autonomy={'done': Autonomy.Off})
+
+
+        return _state_machine
+
+
+    # Private functions can be added inside the following tags
+    # [MANUAL_FUNC]
+    
+    # [/MANUAL_FUNC]
