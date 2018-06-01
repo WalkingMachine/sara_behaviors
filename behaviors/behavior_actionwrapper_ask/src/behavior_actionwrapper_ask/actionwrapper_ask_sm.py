80496a141e5c2adc2a383edebf28f5273cab554b
QuentinGaillot <gaillot.q@gmail.com>
2018-05-31 20:39:44 -0400

    premier jet action wrapper ask

--- behaviors/behavior_actionwrapper_ask/src/behavior_actionwrapper_ask/actionwrapper_ask_sm.py
+++ behaviors/behavior_actionwrapper_ask/src/behavior_actionwrapper_ask/actionwrapper_ask_sm.py
@@ -8,7 +8,9 @@
 
 import roslib; roslib.load_manifest('behavior_actionwrapper_ask')
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
-from flexbe_states.wait_state import WaitState
+from flexbe_states.calculation_state import CalculationState
+from sara_flexbe_states.sara_say_key import SaraSayKey
+from sara_flexbe_states.sara_say import SaraSay
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -46,8 +48,9 @@ class ActionWrapper_AskSM(Behavior):
 
 
 	def create(self):
-		# x:30 y:324, x:130 y:324
-		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed', 'critical_fail'])
+		# x:793 y:463, x:150 y:458, x:458 y:463
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed', 'critical_fail'], input_keys=['Action'])
+		_state_machine.userdata.Action = "Default question"
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -56,11 +59,25 @@ class ActionWrapper_AskSM(Behavior):
 
 
 		with _state_machine:
-			# x:89 y:97
-			OperatableStateMachine.add('dummy wait',
-										WaitState(wait_time=1),
-										transitions={'done': 'finished'},
+			# x:30 y:40
+			OperatableStateMachine.add('fisrtSentence',
+										SaraSay(sentence="Hello, I have a question for you.", emotion=1, block=True),
+										transitions={'done': 'trouveLaQuestion'},
 										autonomy={'done': Autonomy.Off})
+
+			# x:387 y:166
+			OperatableStateMachine.add('AskTheQuestion',
+										SaraSayKey(Format=lambda x: x, emotion=1, block=True),
+										transitions={'done': 'AskTheQuestion'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'sentence': 'question'})
+
+			# x:194 y:99
+			OperatableStateMachine.add('trouveLaQuestion',
+										CalculationState(calculation=lambda x: x[1]),
+										transitions={'done': 'AskTheQuestion'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'input_value': 'Action', 'output_value': 'question'})
 
 
 		return _state_machine
