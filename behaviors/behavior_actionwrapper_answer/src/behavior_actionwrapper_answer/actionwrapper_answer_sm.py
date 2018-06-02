a96ba9f7c228b3cc8c7b9c64b17ae3237daff8f2
QuentinGaillot <gaillot.q@gmail.com>
2018-06-02 15:01:42 -0400

    debut answer

--- behaviors/behavior_actionwrapper_answer/src/behavior_actionwrapper_answer/actionwrapper_answer_sm.py
+++ behaviors/behavior_actionwrapper_answer/src/behavior_actionwrapper_answer/actionwrapper_answer_sm.py
@@ -8,7 +8,12 @@
 
 import roslib; roslib.load_manifest('behavior_actionwrapper_answer')
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
-from flexbe_states.wait_state import WaitState
+from sara_flexbe_states.sara_say import SaraSay
+from behavior_action_findperson.action_findperson_sm import Action_findPersonSM
+from sara_flexbe_states.SetKey import SetKey
+from sara_flexbe_states.get_speech import GetSpeech
+from sara_flexbe_states.for_loop import ForLoop
+from flexbe_states.calculation_state import CalculationState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -32,6 +37,7 @@ class ActionWrapper_AnswerSM(Behavior):
 		# parameters of this behavior
 
 		# references to used behaviors
+		self.add_behavior(Action_findPersonSM, 'Action_findPerson')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -40,13 +46,13 @@ class ActionWrapper_AnswerSM(Behavior):
 
 		# Behavior comments:
 
-		# O 160 33 
+		# O 336 25 
 		# ["Answer"]|nIl faut demander à se faire poser la question.
 
 
 
 	def create(self):
-		# x:30 y:324, x:130 y:324
+		# x:748 y:626, x:66 y:470, x:371 y:623
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed', 'critical_fail'])
 
 		# Additional creation code can be added inside the following tags
@@ -56,10 +62,63 @@ class ActionWrapper_AnswerSM(Behavior):
 
 
 		with _state_machine:
-			# x:89 y:97
-			OperatableStateMachine.add('dummy wait',
-										WaitState(wait_time=1),
+			# x:30 y:40
+			OperatableStateMachine.add('SetPersonClass',
+										SetKey(Value="person"),
+										transitions={'done': 'Action_findPerson'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'Key': 'personClass'})
+
+			# x:20 y:117
+			OperatableStateMachine.add('Action_findPerson',
+										self.use_behavior(Action_findPersonSM, 'Action_findPerson'),
+										transitions={'done': 'AskIfQuestion', 'pas_done': 'noPerson'},
+										autonomy={'done': Autonomy.Inherit, 'pas_done': Autonomy.Inherit},
+										remapping={'className': 'personClass', 'entity': 'entity'})
+
+			# x:45 y:227
+			OperatableStateMachine.add('noPerson',
+										SaraSay(sentence="I can't find any person here.", emotion=1, block=True),
+										transitions={'done': 'failed'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:409 y:108
+			OperatableStateMachine.add('getResponse',
+										GetSpeech(watchdog=7),
+										transitions={'done': 'calculResponse', 'nothing': 'looping', 'fail': 'looping'},
+										autonomy={'done': Autonomy.Off, 'nothing': Autonomy.Off, 'fail': Autonomy.Off},
+										remapping={'words': 'response'})
+
+			# x:386 y:242
+			OperatableStateMachine.add('looping',
+										ForLoop(repeat=1),
+										transitions={'do': 'sayRepeat', 'end': 'NotUnderstandEnd'},
+										autonomy={'do': Autonomy.Off, 'end': Autonomy.Off},
+										remapping={'index': 'index'})
+
+			# x:256 y:224
+			OperatableStateMachine.add('sayRepeat',
+										SaraSay(sentence="I did'nt understand. Do you have a question for me?", emotion=1, block=True),
+										transitions={'done': 'getResponse'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:195 y:357
+			OperatableStateMachine.add('NotUnderstandEnd',
+										SaraSay(sentence="Sorry. I can't understand your response.", emotion=1, block=True),
+										transitions={'done': 'failed'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:574 y:117
+			OperatableStateMachine.add('calculResponse',
+										CalculationState(calculation=lambda x: "yes" in x),
 										transitions={'done': 'finished'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'input_value': 'response', 'output_value': 'output_value'})
+
+			# x:243 y:116
+			OperatableStateMachine.add('AskIfQuestion',
+										SaraSay(sentence="Hello. Do you have a question for me?", emotion=1, block=True),
+										transitions={'done': 'getResponse'},
 										autonomy={'done': Autonomy.Off})
 
 
