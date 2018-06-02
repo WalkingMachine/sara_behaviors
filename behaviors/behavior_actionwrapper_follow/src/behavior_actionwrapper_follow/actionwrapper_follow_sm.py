3d2b9c6fafb4837bb9a31eca9c02a1405d053bd9
Lucas Maurice <lucas.maurice@outlook.com>
2018-06-02 19:17:15 -0400

    Add base rotation to the person to follow.

--- behaviors/behavior_actionwrapper_follow/src/behavior_actionwrapper_follow/actionwrapper_follow_sm.py
+++ behaviors/behavior_actionwrapper_follow/src/behavior_actionwrapper_follow/actionwrapper_follow_sm.py
@@ -16,6 +16,7 @@ from sara_flexbe_states.sara_say import SaraSay
 from behavior_action_follow.action_follow_sm import Action_followSM
 from sara_flexbe_states.get_speech import GetSpeech
 from flexbe_states.check_condition_state import CheckConditionState
+from behavior_action_lookatfacebase.action_lookatfacebase_sm import action_lookAtFaceBaseSM
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -40,6 +41,7 @@ class ActionWrapper_FollowSM(Behavior):
 
 		# references to used behaviors
 		self.add_behavior(Action_followSM, 'Follow Loop/Action_follow')
+		self.add_behavior(action_lookAtFaceBaseSM, 'action_lookAtFaceBase')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -63,7 +65,7 @@ class ActionWrapper_FollowSM(Behavior):
 
 
 	def create(self):
-		# x:705 y:189, x:705 y:133, x:709 y:277
+		# x:763 y:423, x:705 y:133, x:896 y:268
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed', 'critical_fail'], input_keys=['Action'])
 		_state_machine.userdata.Action = ["Follow", "rachel"]
 		_state_machine.userdata.distance = 1
@@ -92,21 +94,21 @@ class ActionWrapper_FollowSM(Behavior):
 										remapping={'input_value': 'words'})
 
 
-		# x:460 y:132, x:130 y:368, x:468 y:41, x:330 y:368
+		# x:395 y:153, x:390 y:47, x:359 y:362, x:304 y:368, x:430 y:368
 		_sm_follow_loop_1 = ConcurrencyContainer(outcomes=['finished', 'error'], input_keys=['ID', 'distance'], conditions=[
-										('error', [('Action_follow', 'failed')]),
-										('finished', [('Wait Stop', 'finished')])
+										('finished', [('Wait Stop', 'finished')]),
+										('error', [('Action_follow', 'failed')])
 										])
 
 		with _sm_follow_loop_1:
-			# x:195 y:36
+			# x:190 y:37
 			OperatableStateMachine.add('Action_follow',
 										self.use_behavior(Action_followSM, 'Follow Loop/Action_follow'),
 										transitions={'failed': 'error'},
 										autonomy={'failed': Autonomy.Inherit},
 										remapping={'ID': 'ID', 'distance': 'distance'})
 
-			# x:203 y:126
+			# x:190 y:139
 			OperatableStateMachine.add('Wait Stop',
 										_sm_wait_stop_0,
 										transitions={'finished': 'finished'},
@@ -143,7 +145,7 @@ class ActionWrapper_FollowSM(Behavior):
 										autonomy={'done': Autonomy.Off},
 										remapping={'sentence': 'name'})
 
-			# x:99 y:412
+			# x:276 y:424
 			OperatableStateMachine.add('Tell Way',
 										SaraSay(sentence="Show me the way !", emotion=1, block=True),
 										transitions={'done': 'Follow Loop'},
@@ -152,23 +154,37 @@ class ActionWrapper_FollowSM(Behavior):
 			# x:97 y:324
 			OperatableStateMachine.add('Tell Follow',
 										SaraSayKey(Format=lambda x: "I will follow you, " + x + " !", emotion=1, block=True),
-										transitions={'done': 'Tell Way'},
+										transitions={'done': 'action_lookAtFaceBase'},
 										autonomy={'done': Autonomy.Off},
 										remapping={'sentence': 'name'})
 
-			# x:414 y:404
+			# x:385 y:405
 			OperatableStateMachine.add('Follow Loop',
 										_sm_follow_loop_1,
-										transitions={'finished': 'finished', 'error': 'Cant Follow'},
+										transitions={'finished': 'Stop Follow', 'error': 'Cant Follow'},
 										autonomy={'finished': Autonomy.Inherit, 'error': Autonomy.Inherit},
 										remapping={'ID': 'personId', 'distance': 'distance'})
 
 			# x:416 y:250
 			OperatableStateMachine.add('Cant Follow',
-										SaraSayKey(Format=lambda x: "I can't follow " + x + " !", emotion=1, block=True),
+										SaraSayKey(Format=lambda x: "I can't follow you, " + x + " !", emotion=1, block=True),
 										transitions={'done': 'failed'},
 										autonomy={'done': Autonomy.Off},
 										remapping={'sentence': 'name'})
+
+			# x:574 y:411
+			OperatableStateMachine.add('Stop Follow',
+										SaraSayKey(Format=lambda x: "Ok " + x + ", I will stop to follow you !", emotion=1, block=True),
+										transitions={'done': 'finished'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'sentence': 'name'})
+
+			# x:53 y:409
+			OperatableStateMachine.add('action_lookAtFaceBase',
+										self.use_behavior(action_lookAtFaceBaseSM, 'action_lookAtFaceBase'),
+										transitions={'finished': 'Tell Way', 'failed': 'Say Lost'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'Entity': 'Entity'})
 
 
 		return _state_machine
