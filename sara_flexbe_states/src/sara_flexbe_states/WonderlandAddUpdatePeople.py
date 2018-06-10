e564e11cd3db7bc04ff3f5a38d1e9b0325d59465
Lucas Maurice <lucas.maurice@outlook.com>
2018-06-10 15:17:08 -0400

    Finish the state for add or update people in wonderland.

--- sara_flexbe_states/src/sara_flexbe_states/WonderlandAddUpdatePeople.py
+++ sara_flexbe_states/src/sara_flexbe_states/WonderlandAddUpdatePeople.py
@@ -4,8 +4,12 @@
 import json
 
 import requests
+from flexbe_core.proxy import ProxySubscriberCached
 from flexbe_core import EventState, Logger
-
+from sara_msgs.msg import Entities
+from geometry_msgs.msg import Pose
+from tf.transformations import euler_from_quaternion
+from math import tan
 """
 Created on 17/05/2018
 
@@ -25,12 +29,63 @@ class WonderlandAddUpdatePeople(EventState):
         super(WonderlandAddUpdatePeople, self).__init__(outcomes=['done'])
         self.url = "http://wonderland:8000/api/people/"
 
+        self._sub = ProxySubscriberCached({'/entities': Entities})
+
+        self._topic = "/robot_pose"
+        self._subscriber_pos = ProxySubscriberCached({self._topic: Pose})
+        self.my_pose = None
+        self.message = None
+
     def execute(self, userdata):
-        # Generate URL to contact
-        s = 0
+
+        if self._subscriber_pos.has_msg(self._topic):
+            self.my_pose = self._subscriber_pos.get_last_msg(self._topic)
+
+        if self._sub.has_msg('/entities'):
+            Logger.loginfo('getting message')
+            self.message = self._sub.get_last_msg('/entities')
+            self._sub.remove_last_msg('/entities')
+
+        if self.message is not None and self.my_pose is not None:
+            found_entities = self.list()
+
+            for person in found_entities:
+                result = self.add_person(person)
+
+                # If people already exist
+                if result == 1:
+                    result = self.update_person(person)
+
+                    # Error during person update.
+                    if result != 0:
+                        Logger.logerr("An error happen during people update.")
+
+                # Error during person add.
+                elif result < 0:
+                    Logger.logerr("An error happen during people add.")
+
+        return 'done'
+
+    def list_persons(self):
+        found_entities = []
+        wraps = []
+        for entity in self.message.entities:
+            if entity.name == "person":
+                wrap = Wrapper(self.my_pose, entity, 0.5)
+
+                wraps.append(wrap)
+
+        wraps.sort(key=Wrapper.key)
+
+        for wrap in wraps:
+            found_entities.append(wrap.entity)
+
+        return found_entities
 
     def add_person(self, entity):
         data = {}
+
+        # Prepare request
 
         if entity.face.id is not None:
             data.update({'peopleRecognitionId': entity.face.id})
@@ -98,6 +153,8 @@ class WonderlandAddUpdatePeople(EventState):
     def update_person(self, entity):
         data = {}
 
+        # Prepare request
+
         if entity.wonderlandId is None and entity.face.id is None:
             Logger.logwarn('Need wonderland ID or face ID !')
             return 'bad_request'
@@ -158,3 +215,27 @@ class WonderlandAddUpdatePeople(EventState):
         except requests.exceptions.RequestException as e:
             Logger.logerr(e)
             return -1
+
+
+class Wrapper:
+    def __init__(self, my_pose, entity, frontality_level):
+
+        self.entity = entity
+
+        x = entity.position.x - my_pose.position.x
+        y = entity.position.y - my_pose.position.y
+
+        quaternion = [my_pose.orientation.x, my_pose.orientation.y, my_pose.orientation.z, my_pose.orientation.w]
+        euler = euler_from_quaternion(quaternion)
+        A = euler[2]
+
+        a = tan(A)
+        b = y - x * a
+
+        self.dist = (abs(y - a * x - b) / (1 + b ** 2) ** 0.5) * frontality_level
+        self.dist += (((entity.position.x - my_pose.position.x) ** 2 + (
+                entity.position.y - my_pose.position.y) ** 2) ** 0.5) * (1 - frontality_level)
+        self.dist /= entity.probability**2
+
+    def key(self):
+        return self.dist
