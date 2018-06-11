530844a16a573ab49850a22631f97d8ad89465c9
philippe <philippe117@live.fr>
2018-06-10 20:10:44 -0400

    Clean Up NLU state

--- sara_flexbe_states/src/sara_flexbe_states/sara_nlu_spr.py
+++ sara_flexbe_states/src/sara_flexbe_states/sara_nlu_spr.py
@@ -3,48 +3,42 @@
 from __future__ import print_function
 from flexbe_core import EventState, Logger
 import rospy
-import re
 from wm_nlu.srv import AnswerQuestion
 from std_msgs.msg import String
 
 
 class SaraNLUspr(EventState):
     '''
-    Use wm_nlu to parse a sentence and return the detected actions in a standard format (ActionForm)
+    Use wm_nlu to parse a sentence and return the answer.
+    ># sentence         string      sentence to parse
+    #> answer           string      answer
 
-    ># sentence     string      sentence to parse
-
-    #> ActionForms     string[]      list of ActionForms
-
-    <= understood     Finished job.
-    <= not_understood     Finished job but no commands detected.
+    <= understood       Finished job.
+    <= not_understood   Finished job but no commands detected.
     <= fail     service unavailable.
     '''
 
     def __init__(self):
         # See example_state.py for basic explanations.
         super(SaraNLUspr, self).__init__(outcomes=['understood', 'not_understood', 'fail'], input_keys=['sentence'],
-                                      output_keys=['answer'])
-        self.RecurentSubject = None
-        self.Person = None
-        self.serviceName = "/answer_question"
+                                         output_keys=['answer'])
 
-        Logger.loginfo("waiting forservice: " + self.serviceName)
-        rospy.wait_for_service(self.serviceName)
+        serviceName = "/answer_question"
+
+        Logger.loginfo("waiting forservice: " + serviceName)
+        rospy.wait_for_service(serviceName)
+
+        self.service = rospy.ServiceProxy(serviceName, AnswerQuestion)
 
     def execute(self, userdata):
 
         # Call the NLU service
-        serv = rospy.ServiceProxy(self.serviceName, AnswerQuestion)
-        Resp = serv(String(userdata.sentence))
+        response = self.service(String(userdata.sentence))
 
-        # Checking the validity of the responce
-        if Resp.str.data is "":
-            userdata.answer = Resp.str
+        # Checking the validity of the response
+        if response.str.data is "":
+            userdata.answer = response.str.data
             return "fail"
 
-        userdata.answer = Resp.str
+        userdata.answer = response.str.data
         return "understood"
-
-    def on_enter(self, userdata):
-        Logger.loginfo('Enter SaraNLU')
