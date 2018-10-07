a342721c9f2f4bcedfaef8ffa22c96c12cbf6862
Huynh-Anh Le <huynh-anh.le.1@ens.etsmtl.ca>
2018-10-06 21:10:32 -0400

    Passage vers proxy pour sub, pub et service

--- sara_flexbe_states/src/sara_flexbe_states/sara_say.py
+++ sara_flexbe_states/src/sara_flexbe_states/sara_say.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-from flexbe_core import EventState, Logger
+from flexbe_core.proxy import ProxyPublisher
+from flexbe_core.proxy import ProxySubscriberCached
+from flexbe_core.proxy import ProxyServiceCaller
+from flexbe_core.proxy import ProxyActionClient
+
 import rospy
 from wm_tts.msg import say
 from wm_tts.srv import say_service
@@ -33,26 +37,35 @@ class SaraSay(EventState):
         self.emotion_topic = "sara_face/Emotion"
         self.say_topic = "/say"
         self.say_service = "/wm_say"
-        self.emotion_pub = rospy.Publisher(self.emotion_topic, UInt8, queue_size=1)
+        self.emotion_pub = ProxyPublisher({self.emotion_topic: UInt8})
 
         # Prepare wm_tts
         if self.block:
             Logger.loginfo('Waiting for wm_tts')
             rospy.wait_for_service(self.say_service)
-            self.serv = rospy.ServiceProxy(self.say_service, say_service)
+            self.serv = ProxyServiceCaller({self.say_service: say_service})
         else:
-            self.pub = rospy.Publisher(self.say_topic, say, queue_size=1)
+            self.pub = ProxyPublisher({self.say_topic: say})
 
     def execute(self, userdata):
         """Wait for action result and return outcome accordingly"""
         if(self.msg.emotion>0 and self.msg.emotion != self.lastEmotion):
-            self.emotion_pub.publish(self.msg.emotion)
+            self.emotion_pub.publish(self.emotion_topic, self.msg.emotion)
             self.lastEmotion = self.msg.emotion
 
         Logger.loginfo('Say: '+str(self.msg))
         if self.block:
-            self.serv(self.msg)
+            # before:
+            # self.serv(self.msg)
+
+            # after:
+            try:
+                self._response = self.serv.call(self.say_service, self.msg)
+                Logger.loginfo("Execute Known Trajectory Service requested: \n" + str(self.say_service))
+            except rospy.ServiceException as exc:
+                Logger.logwarn("Execute Known Trajectory Service did not process request: \n" + str(exc))
+
         else:
-            self.pub.publish(self.msg)
+            self.pub.publish(self.say_topic, self.msg)
 
         return 'done'
