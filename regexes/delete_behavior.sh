904c2ac7d52eecab368f29b633fdfedcfad51bd7
philippe <philippe117@live.fr>
2019-01-18 15:54:12 -0500

    paufine le script de deletion

--- regexes/delete_behavior.sh
+++ regexes/delete_behavior.sh
@@ -2,12 +2,18 @@
 
 DEPFILE="regexes/graphs/dependenceGraph.plantuml"
 
+gawk -f regexes/generateDependenciesGraph.gawk  behaviors/behavior_action_*/src/behavior_*/*.py  > /dev/null && sort -u regexes/graphs/dependenceGraph.plantuml > /dev/null
+
+
+OCCURENCE="$(grep -ci "$1 -> " "$DEPFILE")"
+echo $OCCURENCE
+
 # If the behavior exist
 if [ -d "behaviors/behavior_$1" ]
 then
 
     # If the file is in the deppendency file
-    if [[ 0 = $(grep -c "$1" "$DEPFILE") ]]
+    if [[ 0 = $OCCURENCE ]]
     then
 
         echo "deleting: behaviors/behavior_$1"
@@ -19,10 +25,12 @@ then
         echo "deleting: behavior_$1 from package.xml"
         sed -i "/$1/d" flexbe_behaviors/package.xml
 
+        echo "$1 deleting: behavior_$1 from package.xml"
+
     else
 
         echo "behavior_$1 is still in use"
-        grep "$1" "$DEPFILE"
+        grep "$1 => " "$DEPFILE"
 
     fi
 else
