1b4ca5f938990a5302bcf29aadf0fa564a1b417b
philippe <philippe117@live.fr>
2019-01-21 21:31:43 -0500

    ajoute les manifests.xml

--- sara_flexbe_behaviors/src/sara_flexbe_behaviors/test_de_behavior_sm.py
+++ sara_flexbe_behaviors/src/sara_flexbe_behaviors/test_de_behavior_sm.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 ###########################################################
 #               WARNING: Generated code!                  #
 #              **************************                 #
@@ -6,9 +7,8 @@
 # Only code inside the [MANUAL] tags will be kept.        #
 ###########################################################
 
-from flexbe_core import Behavior, Autonomy, OperatableStateMachine, Logger
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
 from flexbe_states.log_state import LogState
-from flexbe_states.wait_state import WaitState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -16,21 +16,20 @@ from flexbe_states.wait_state import WaitState
 
 
 '''
-Created on Fri Aug 21 2015
-@author: Philipp Schillinger
+Created on Mon Jan 21 2019
+@author: philippe
 '''
-class ExampleBehaviorSM(Behavior):
+class testdebehaviorSM(Behavior):
 	'''
-	This is a simple example for a behavior.
+	une behavior de test
 	'''
 
 
 	def __init__(self):
-		super(ExampleBehaviorSM, self).__init__()
-		self.name = 'Example Behavior'
+		super(testdebehaviorSM, self).__init__()
+		self.name = 'test de behavior'
 
 		# parameters of this behavior
-		self.add_parameter('waiting_time', 3)
 
 		# references to used behaviors
 
@@ -41,15 +40,11 @@ class ExampleBehaviorSM(Behavior):
 
 		# Behavior comments:
 
-		# O 172 147 
-		# This transition will only be executed if the Autonomy Level is greater than Low during execution, e.g. High
-
 
 
 	def create(self):
-		log_msg = "Hello World!"
-		# x:83 y:390
-		_state_machine = OperatableStateMachine(outcomes=['finished'])
+		# x:255 y:192, x:130 y:324
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -58,15 +53,9 @@ class ExampleBehaviorSM(Behavior):
 
 
 		with _state_machine:
-			# x:52 y:78
-			OperatableStateMachine.add('Print_Message',
-										LogState(text=log_msg, severity=Logger.REPORT_HINT),
-										transitions={'done': 'Wait_After_Logging'},
-										autonomy={'done': Autonomy.Low})
-
-			# x:40 y:228
-			OperatableStateMachine.add('Wait_After_Logging',
-										WaitState(wait_time=self.waiting_time),
+			# x:72 y:118
+			OperatableStateMachine.add('log',
+										LogState(text="test", severity=Logger.REPORT_HINT),
 										transitions={'done': 'finished'},
 										autonomy={'done': Autonomy.Off})
 
