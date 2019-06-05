3715be413d781c85b050cc9a1195019ff9d47f99
QuentinGaillot <gaillot.q@gmail.com>
2019-06-04 20:39:22 -0400

    fix, retourne un bon point :)

--- sara_flexbe_states/src/sara_flexbe_states/GetPositionToPlaceOnTable.py
+++ sara_flexbe_states/src/sara_flexbe_states/GetPositionToPlaceOnTable.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-
+import string
 import math
 from flexbe_core import EventState, Logger
 from flexbe_core.proxy import ProxySubscriberCached
-from geometry_msgs.msg import Point
-from sensor_msgs.msg import PointCloud
+from geometry_msgs.msg import Point, Pose
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs import point_cloud2
 
 '''
 Created on 03.06.2019
@@ -28,8 +29,8 @@ class GetPositionToPlaceOnTable(EventState):
         '''
         super(GetPositionToPlaceOnTable, self).__init__(outcomes=['done', 'not_found'], input_keys=['distanceFromEdge'], output_keys=['position'])
 
-        self.planeTopic = "/plane"
-        self.planeSub = ProxySubscriberCached({self.planeTopic: PointCloud})
+        self.planeTopic = "/segment_table/plane"
+        self.planeSub = ProxySubscriberCached({self.planeTopic: PointCloud2})
 
         self.robotposeTopic = "/robot_pose"
         self.robotposeSub = ProxySubscriberCached({self.robotposeTopic: Pose})
@@ -56,15 +57,29 @@ class GetPositionToPlaceOnTable(EventState):
 
         if self.plane is not None and mypose is not None:
 
-            closest_point = self.plane.points[0]
+            gen = point_cloud2.read_points(self.plane)
+
+            closest_point = Point()
+            closest_point.x = 0
+            closest_point.y = 0
+            closest_point.z = 0
             min_dist = 99999
+            numberOfPoints = 0
 
             sum_x = 0
             sum_y = 0
             sum_z = 0
 
             # find the closest point and center
-            for point in self.plane:
+            point = Point()
+
+            for p in gen:
+                numberOfPoints = numberOfPoints + 1
+
+                point.x = p[0]
+                point.y = p[1]
+                point.z = p[2]
+
                 sum_x += point.x
                 sum_y += point.y
                 sum_z += point.z
@@ -74,9 +89,9 @@ class GetPositionToPlaceOnTable(EventState):
                     closest_point = point
 
             center = Point()
-            center.x = sum_x / len(self.plane.points)
-            center.y = sum_y / len(self.plane.points)
-            center.z = sum_z / len(self.plane.points)
+            center.x = sum_x / numberOfPoints
+            center.y = sum_y / numberOfPoints
+            center.z = sum_z / numberOfPoints
 
             #find point to return
             distanceClosestPointCenter = self.dist(closest_point, center)
@@ -92,3 +107,9 @@ class GetPositionToPlaceOnTable(EventState):
                 userdata.position = pointToReturn
 
                 return "done"
+
+        else:
+            if self.plane is None:
+                Logger.loginfo('plane is none')
+            if mypose is  None:
+                Logger.loginfo('pose is none')
