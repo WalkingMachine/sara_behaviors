4a40f34c4498fa4cd3f58b557d46e1b9b8800361
QuentinGaillot <gaillot.q@gmail.com>
2019-06-06 23:15:29 -0400

    calcul du pitch et yaw, reste le roll a faire

--- sara_flexbe_states/src/sara_flexbe_states/GetGraspFromEntity.py
+++ sara_flexbe_states/src/sara_flexbe_states/GetGraspFromEntity.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 from flexbe_core import EventState, Logger
 import rospy
 import copy
@@ -11,7 +12,7 @@ import numpy as np
 from scipy.linalg import lstsq
 from gpd.msg import CloudIndexed
 from std_msgs.msg import Header, Int64
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, Pose
 from sara_msgs.msg import Entity, Entities
 from gpd.msg import GraspConfigList
 
@@ -32,12 +33,12 @@ class GetGraspFromEntity(EventState):
     '''
 
     def graspCallback(self, msg):
-        self.grasp = msg.grasps[0]
+        self.graspList = msg.grasps
 
     def __init__(self):
         super(GetGraspFromEntity, self).__init__(outcomes=['done', 'failed'], input_keys=['Entity'], output_keys=['GraspingPose'])
         #self.emotion=emotion
-        self.grasp = None
+        self.graspList = None
         self.grasps_sub = rospy.Subscriber('/detect_grasps/clustered_grasps', GraspConfigList, self.graspCallback)
 
     def on_enter(self, userdata):
@@ -83,93 +84,20 @@ class GetGraspFromEntity(EventState):
 
         ################################
         # Temporary setting a timeout
-        while self.grasp == None:
+        while self.graspList == None:
             i = i+1
             rospy.sleep(1)
             if i > 20:
                 return 'failed'
 
-        userdata.GraspingPose = self.grasp
-        Logger.loginfo(self.grasp)
 
+        for grasp in self.graspList:
+            pose = Pose()
+            pose.Point = grasp.top
+            yaw = atan2(grasp.approach.y , grasp.approach.x)
+            pitch = atan2( grasp.approach.z , sqrt((grasp.approach.x*grasp.approach.x)+(grasp.approach.y*grasp.approach.y)) )
+            roll = 0
 
-        topic = 'visualization_marker_array'
-        marker_publisher = rospy.Publisher(topic, MarkerArray)
-
-        markerArray = MarkerArray()
-        marker = Marker()
-        marker.header.frame_id = "/base_link"
-        marker.type = marker.SPHERE
-        marker.scale.x = 0.02
-        marker.scale.y = 0.02
-        marker.scale.z = 0.02
-        marker.action = marker.ADD
-        marker.color.a = 1.0
-        marker.color.r = 1.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
-        marker.pose.orientation.w = 1.0
-
-        marker1 = copy.deepcopy(marker)
-        marker1.id = 1
-        marker1.pose.position.x = self.grasp.bottom.x
-        marker1.pose.position.y = self.grasp.bottom.y
-        marker1.pose.position.z = self.grasp.bottom.z
- 
-        marker2 = copy.deepcopy(marker)
-        marker2.id = 2
-        marker2.pose.position.x = self.grasp.top.x
-        marker2.pose.position.y = self.grasp.top.y
-        marker2.pose.position.z = self.grasp.top.z
-
-        marker3 = copy.deepcopy(marker)
-        marker3.id = 3
-        marker3.pose.position.x = self.grasp.surface.x
-        marker3.pose.position.y = self.grasp.surface.y
-        marker3.pose.position.z = self.grasp.surface.z
-
-        marker4 = copy.deepcopy(marker)
-        marker4.id = 4
-        marker4.pose.position.x = self.grasp.surface.x
-        marker4.pose.position.y = self.grasp.surface.y
-        marker4.pose.position.z = self.grasp.surface.z
-
-        marker5 = copy.deepcopy(marker)
-        marker5.id = 5
-        marker5.pose.position.x = self.grasp.approach.x
-        marker5.pose.position.y = self.grasp.approach.y
-        marker5.pose.position.z = self.grasp.approach.z
-
-        marker6 = copy.deepcopy(marker)
-        marker6.id = 6
-        marker6.pose.position.x = self.grasp.binormal.x
-        marker6.pose.position.y = self.grasp.binormal.y
-        marker6.pose.position.z = self.grasp.binormal.z
-
-        marker7 = copy.deepcopy(marker)
-        marker7.id = 7
-        marker7.pose.position.x = self.grasp.axis.x
-        marker7.pose.position.y = self.grasp.axis.y
-        marker7.pose.position.z = self.grasp.axis.z
-
-        marker8 = copy.deepcopy(marker)
-        marker8.id = 8
-        marker8.pose.position.x = self.grasp.sample.x
-        marker8.pose.position.y = self.grasp.sample.y
-        marker8.pose.position.z = self.grasp.sample.z
-
-        markerArray.markers.append(marker1)
-        markerArray.markers.append(marker2)
-        markerArray.markers.append(marker3)
-        markerArray.markers.append(marker4)
-        markerArray.markers.append(marker5)
-        markerArray.markers.append(marker6)
-        markerArray.markers.append(marker7)
-        markerArray.markers.append(marker8)
-
-
-
-        marker_publisher.publish(markerArray)
 
         return 'done'
         #return 'failed'
