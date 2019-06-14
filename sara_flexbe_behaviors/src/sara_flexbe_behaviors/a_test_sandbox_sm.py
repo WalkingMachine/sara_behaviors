06668efa9fa6a538809a0ba7f141c23e662b07c6
philippe <philippe117@live.fr>
2019-06-14 16:57:15 -0400

    commit

--- sara_flexbe_behaviors/src/sara_flexbe_behaviors/a_test_sandbox_sm.py
+++ sara_flexbe_behaviors/src/sara_flexbe_behaviors/a_test_sandbox_sm.py
@@ -8,8 +8,10 @@
 ###########################################################
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
-from sara_flexbe_behaviors.test_de_behavior_sm import testdebehaviorSM as sara_flexbe_behaviors__testdebehaviorSM
+from sara_flexbe_states.pose_gen_euler import GenPoseEuler
 from flexbe_states.log_state import LogState
+from sara_flexbe_states.moveit_moveCartesian import MoveitMoveCartesian
+from sara_flexbe_states.moveit_move import MoveitMove
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -33,7 +35,6 @@ class ATestSandboxSM(Behavior):
 		# parameters of this behavior
 
 		# references to used behaviors
-		self.add_behavior(sara_flexbe_behaviors__testdebehaviorSM, 'test de behavior')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -45,7 +46,7 @@ class ATestSandboxSM(Behavior):
 
 
 	def create(self):
-		# x:537 y:110, x:515 y:330
+		# x:537 y:110, x:166 y:479
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.Pose1 = "PostGripPose"
 		_state_machine.userdata.Pose2 = "IdlePose"
@@ -64,17 +65,39 @@ class ATestSandboxSM(Behavior):
 
 
 		with _state_machine:
-			# x:134 y:83
-			OperatableStateMachine.add('test de behavior',
-										self.use_behavior(sara_flexbe_behaviors__testdebehaviorSM, 'test de behavior'),
-										transitions={'finished': 'log', 'failed': 'failed'},
-										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+			# x:47 y:88
+			OperatableStateMachine.add('gen',
+										GenPoseEuler(x=0.8, y=0, z=1.1, roll=0, pitch=0, yaw=0),
+										transitions={'done': 'gen2'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'pose': 'pose1'})
 
 			# x:411 y:111
 			OperatableStateMachine.add('log',
 										LogState(text="ok", severity=Logger.REPORT_HINT),
 										transitions={'done': 'finished'},
 										autonomy={'done': Autonomy.Off})
+
+			# x:302 y:211
+			OperatableStateMachine.add('mp',
+										MoveitMoveCartesian(move=True, waitForExecution=True, group="RightArm"),
+										transitions={'done': 'log', 'failed': 'failed'},
+										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'targetPose': 'pose2'})
+
+			# x:61 y:235
+			OperatableStateMachine.add('move',
+										MoveitMove(move=True, waitForExecution=True, group="RightArm"),
+										transitions={'done': 'mp', 'failed': 'failed'},
+										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'target': 'pose1'})
+
+			# x:203 y:94
+			OperatableStateMachine.add('gen2',
+										GenPoseEuler(x=0.8, y=-0.5, z=1.1, roll=0, pitch=0, yaw=0),
+										transitions={'done': 'move'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'pose': 'pose2'})
 
 
 		return _state_machine
