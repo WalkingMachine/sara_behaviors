5ea3db4ec7585f84f3a3b87e9e040b7fd0baa433
Huynh-Anh <huynh-anh.le.1@ens.etsmtl.ca>
2020-02-22 15:39:49 -0500

    get pointcloud test1

---
+++ sara_flexbe_states/src/sara_flexbe_states/talker.py
@@ -0,0 +1,32 @@
+
+import rospy
+from sara_msgs import msg
+from flexbe_core import EventState, Logger
+
+class handleBag(EventState):
+'''
+    ALlows the user to get a pointcloud of the handle
+
+    -- handle  function	     ALlows the user to get a pointcloud of the handle /sort data
+
+    ># input_list  list		Input to the filter function
+
+    #> output_PointCloud PointCloud	The result of the filter.
+
+    <= done					Indicates completion of the filter.
+
+    '''
+	def __init__(self,handle):
+		'''
+	    Constructor
+	    '''
+		super(handle,self).__init__(outcomes=['found','notfound'],input_keys=['input_list'],output_keys=['output_PointCloud'])
+	def handle(self, userdata):
+		list=[]
+		list = list.append(userdata.PointCloud)
+
+	def execute(self,userdata):
+		handle(userdata)
+
+		Logger.loginfo(str(userdata.PointCloud))
+		return 'done'
