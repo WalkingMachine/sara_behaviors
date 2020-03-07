6caa1b4514cd53bed958804fb6560b4428c49e0c
philippe <philippe117@live.fr>
2020-03-07 17:57:57 -0500

    changed

--- sara_flexbe_states/src/sara_flexbe_states/talker.py
+++ sara_flexbe_states/src/sara_flexbe_states/talker.py
@@ -2,7 +2,7 @@
 import rospy
 from sara_msgs import msg
 from flexbe_core import EventState, Logger
-
+import ros_numpy
 class handleBag(EventState):
     '''
     ALlows the user to get a pointcloud of the handle
@@ -26,6 +26,8 @@ class handleBag(EventState):
     def execute(self,userdata):
 
         if (len(userdata.entity.pointcloud.data)):
-            return "does_have_handle"
+            xyz_array=ros_numpy.point_cloud2.point_cloud2_to_xyz_array(userdata.entity.pointcloud)
+            xyz_array=np.mean(xyz_array)
+            return xyz_array
         else:
             return "does_not_have_handle"
